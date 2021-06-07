840e1645612e94d5cf06f3c154efa04a5c137240
tarun <tarun.verma151@gmail.com>
2021-06-07 11:15:30 +0530

    bug - update loop

--- 02-data-structure-algorithms-3/004_dynamic_programming_1.cpp
+++ 02-data-structure-algorithms-3/004_dynamic_programming_1.cpp
@@ -71,7 +71,7 @@ int fibonicci2(int n){
     int* arr = new int[n+1];
     arr[0] = 0;
     arr[1] = 1;
-    for(int i=0; i<=n; i++){
+    for(int i=2; i<=n; i++){
         arr[i] = arr[i-1] + arr[i-2];
     }
     return arr[n];

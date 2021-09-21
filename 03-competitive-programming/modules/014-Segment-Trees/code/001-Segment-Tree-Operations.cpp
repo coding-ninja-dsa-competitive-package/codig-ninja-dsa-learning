8332b08d8c0bbdd38f1ce3cee3821a0c49172e84
“tarunve” <tarun.verma151@gmail.com>
2021-09-21 23:58:17 +0530

    cp - segment tree

--- 03-competitive-programming/modules/014-Segment-Trees/code/001-Segment-Tree-Operations.cpp
+++ 03-competitive-programming/modules/014-Segment-Trees/code/001-Segment-Tree-Operations.cpp
@@ -2,6 +2,12 @@
 
 using namespace std;
 
+/**
+ *  -   Segment tree are best suited when we want to solve problems with a range.
+ *  -   Size of Segment tree to be considered:
+ *          2^(ceil(logn)) => 4n
+ */
+
 void buildTree(int* arr, int* tree, int start, int end, int treeNode){
     if(start == end){
         tree[treeNode] = arr[start];
@@ -9,10 +15,8 @@ void buildTree(int* arr, int* tree, int start, int end, int treeNode){
     }
 
     int mid = (start+end)/2;
-
     buildTree(arr, tree, start, mid, 2*treeNode);
     buildTree(arr, tree, mid+1, end, 2*treeNode+1);
-
     tree[treeNode] = tree[2*treeNode] + tree[2*treeNode+1];
 }
 
@@ -31,23 +35,42 @@ void updateTree(int* arr, int* tree, int start, int end, int treeNode, int idx, 
     tree[treeNode] = tree[2*treeNode] + tree[2*treeNode+1];
 }
 
-int main() {
-    int arr[] = {1,2,3,4,5,6,7,8,9};
-    int* tree = new int[10];
+int queryTree(int* tree, int start, int end, int treeNode, int left, int right){
+    //completely outside given range
+    if(start > right || end < left){
+        return 0;
+    }
+    //completely inside given range
+    if(start >= left && end <= right){
+        return tree[treeNode];
+    }
+    //partially inside and partially outside range
+    int mid = (start+end)/2;
+    int ans1 = queryTree(tree, start, mid, 2*treeNode, left, right);
+    int ans2 = queryTree(tree, mid+1, end, 2*treeNode+1, left, right);
+    return ans1+ans2;
+}
 
+int main() {
+    int arr[] = {1,2,3,4,5};
+    int* tree = new int[10];
+    //  Build Tree
     buildTree(arr, tree, 0, 4, 1);
     cout << "build tree : ";
     for(int i=1; i<10; i++){
         cout << tree[i] << " ";
     }
     cout << endl;
-
+    //  Update Tree
     updateTree(arr, tree, 0, 4, 1, 2, 10);
     cout << "update tree : ";
     for(int i=1; i<10; i++){
         cout << tree[i] << " ";
     }
     cout << endl;
+    //  Query Tree
+    int ans = queryTree(tree, 0, 4, 1, 2, 4);
+    cout << "query tree : " << ans << endl;
 
     return 0;
 }

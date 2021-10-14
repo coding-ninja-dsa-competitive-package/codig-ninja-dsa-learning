24811a9dc93318007c75cffee547d2c02063b0e6
“tarunve” <tarun.verma151@gmail.com>
2021-10-14 22:52:54 +0530

    cp - DP and bitmasking problems

---
+++ 03-competitive-programming/modules/019-DP-And-Bitmasking/code/002-Candy.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/**
+ *  Candy
+ *  -   Gary is a teacher at XYZ school. To reward his N students he bought a packet 
+ *      of N candies all with different flavours. But the problem is some students 
+ *      like certain flavour while some doesn't. Now Gary wants to know the number 
+ *      of ways he can distribute these N candies to his N students such that every 
+ *      student gets exactly one candy he likes.
+ *  
+ *  Input Format :
+ *      Line 1 : An integer N (1<= N <= 16) denoting number of students and candies.
+ *      Next N lines : N integers describing the preferences of one student. 1 at 
+ *      i'th (0 <= i < N) position denotes that this student likes i'th candy , 0 
+ *      means he doesn't.
+ *      Assume input to be 0-indexed based.
+ *  Output Format :
+ *      Return the number of ways Gary can distribute these N candies to his N 
+ *      students such that every student gets exactly one candy he likes.``
+ *  
+ *  Sample Input:
+ *      3   
+ *      1 1 1
+ *      1 1 1
+ *      1 1 1
+ *  Sample Output:  
+ *      6
+ */
+#define MAXN 16
+
+int candies(int like[][MAXN], int n, int person, int mask, int* dp){
+    if(person >= n){
+        return 1;
+    }
+    if(dp[mask] != 0){
+        return dp[mask];
+    }
+    int ans=0; 
+    for(int i=0; i<n; i++){
+        if((!(mask & (1<<i))) && like[person][i]){
+        	ans += candies(like, n, person+1, mask|(1<<i), dp); 
+        }
+    }
+	dp[mask] = ans;
+	return ans;
+}
+
+long long solve(int like[][MAXN],int N){
+	int* dp = new int[1<<N]();
+    int ans = candies(like, N , 0 , 0, dp);
+    delete [] dp;
+    return ans;
+}
+
+int main(){
+	int n,like[MAXN][MAXN];
+	cin>>n;
+	for(int i = 0; i < n ; i++){
+		for(int j = 0; j < n; j++){
+			cin>>like[i][j];
+		}
+	}
+	cout<<solve(like,n)<<endl;
+}

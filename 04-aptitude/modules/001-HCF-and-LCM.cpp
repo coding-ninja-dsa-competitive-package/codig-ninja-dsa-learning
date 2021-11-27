e3c140c8e0a9b617c4b287ce0b8ba5be32c059be
“tarunve” <tarun.verma151@gmail.com>
2021-11-27 21:41:33 +0530

    aptitude notes

---
+++ 04-aptitude/modules/001-HCF-and-LCM.cpp
@@ -0,0 +1,270 @@
+/**
+ *  Predict The Number
+ *  -   The product of two positive integers is 216 and sum of the numbers is 30. 
+ *      Which of the following can be one number of the two numbers?
+ *      a) 12  b) 16   c) 24  d) 48
+ *  Ans:    12
+ *      -   Factors are : (2, 108) , (3, 73), (4, 54), (6, 36), (8, 27), (9, 24),
+ *          (12, 18). 
+ *      -   So, with sum 30 - (12, 18) is the pair. 
+ */
+
+/**
+ *  Sum Of Numbers
+ *  -   The product of two positive integers is 240 and the difference of the 
+ *      numbers is 8. Which of the following can be the sum of the two numbers?  
+ *      a) 12  b) 16   c) 24  d) 32
+ *  Ans:    32
+ *      -    Let the two numbers are ‘a’ and ‘b’.
+ *      -   a- b= 8 and a*b= 240
+ *      -   if a*b= 240, then the possible combinations are (1, 240), (2, 120), 
+ *          (3, 80), (4, 60), (5, 48), (6, 40), (8, 30), (10, 24), (12, 20), 
+ *          (15, 16).
+ *      -   a- b= 8 if possible only for (12, 20).
+ *      -   Required sum= 12+ 20= 32. Hence, option (d) is correct.
+ */
+
+/**
+ *  Other Number
+ *  -   The product of two positive integers is 185. If one integer is a prime 
+ *      number greater than 11. Which of the following can be the other number?
+ *      a) 37  b) 185   c) 5  d) 1
+ *  Ans:    5
+ *      -   185= 1* 185=5*37
+ *      -   According to the question one integer is a prime number (greater 
+ *          than 11). It means one factor is 37, other factor must be 5. Hence, 
+ *          option (c) is correct.
+ */
+
+/**
+ *  Prime Number
+ *  -   Which of the following is a prime number?
+ *      a) 147  b) 91   c) 101  d) None of These
+ *  Ans:    101
+ *      -   147 is divisible by 3.91 is divisible by 7 and 13. 101 has no 
+ *          factor except 1 and 101 itself. Hence, option (c) is correct.
+ */
+
+/**
+ *  Predict The Prime Number
+ *  -   Which of the following is a prime number?
+ *      a) 127  b) 141   c) 171  d) All of These
+ *  Ans:    127
+ *      -   11<√127<12, 11<√141<12, 13<√171<14 
+ *      -   For 127, we just need to check whether 127 is divisible by 3, 7, 
+ *          9, 11. 127 is not divisible by none of these four numbers. Hence, 
+ *          this is a prime number. 
+ *      -   But 141 and 171 are not prime numbers.
+ *      -   Hence, option (a) is correct.
+ */
+
+/**
+ *  HCF :   All prime factors with their lowest available powers.
+ *  LCM :   All prime factors with their maximum available powers.
+ */
+
+/**
+ *  Find HCF?
+ *  -   Find the HCF of 2, 4, 8, 12.
+ *      a) 4  b) 6   c) 2  d) 8
+ *  Ans:    2
+ *      -   Factors of 2 are 1, 2
+ *      -   Factors of 4 are 1, 2, 4
+ *      -   Factors of 8 are 1, 2, 4, 8
+ *      -   Factors of 12 are 1, 2, 3, 4, 6, 12
+ *      -   So, the highest common factor = 2 
+ */
+
+/**
+ *  HCF
+ *  -   The HCF of the numbers 27, 81, 72 is__? 
+ *      a) 3  b) 6   c) 9  d) 12
+ *  Ans:    9
+ *      -   27=3^3  
+ *      -   81=3^4        	
+ *      -   72= 3^2×2^3 
+ *      -   Hence, the highest common factor= 3^2=9. Hence, option (c) is correct.
+ */
+
+/**
+ *  HCF Of Numbers
+ *  -   What is the HCF of the numbers 12 * x^3* y^2 * z, 15* x^2 * y^3 * z^2, 
+ *      18 * x^3 * y^2 * z^4
+ *      a) 3*x*y*z  b) 12*x*y*z   c) 3*x^2*y^2*z  d) 12*x^2*y^2*z
+ *  Ans:    3*x^2*y^2*z
+ *      -   12*x^3 *  y^2 * z=2^2 *  3 * x^3  * y^2 * z
+ *      -   15*x^2 * y^3 * z ^2=3 * 5 * x^2 * y^3 * z^2
+ *      -   18*x^3 * y^2 * z^4= 2 * 3^2 * x^2 * y^3 * z^4   
+ *      -   HCF of the three numbers= 3*x^2 *  y^2  * z=3 * x^2  * y^2 * z
+ */
+
+/**
+ *  Predict The HCF
+ *  -   Which of the following number is the HCF of 56 and 68?
+ *      a) 12  b) 4   c) 6  d) None of These
+ *  Ans:    4
+ *      -   Difference of 56 and 68= 12.
+ *      -   Factors of 12 are 12, 6, 4, 3, 2, 1.
+ *      -   12 don’t divide 56 and 68.
+ *      -   6 don’t divide 56 and 68.
+ *      -   4 divide 56 and 68 both.
+ *      -   Hence, 4 is the HCF. Option (b) is correct.
+ */
+
+/**
+ *  HCF Prediction
+ *  -   Which of the following number is the HCF of 136 and 166?
+ *      a) 12  b) 4   c) 6  d) 2
+ *  Ans:   2
+ *      -   Difference of 136 and 166= 30.
+ *      -   Factors of 30 are 30, 15, 10, 6, 5, 3, 2, 1.
+ *      -   30, 15, 10, 6, 3 don’t divide 136 and 166. 4 don’t divide 166.
+ *      -   2 divide both 136 and 166 both. Hence, 2 is the HCF. Option (d) is 
+ *          correct.
+ */
+
+/**
+ *  Maximum Length Of Taps
+ *  -   If the sides of a field are 1410m, 1456m, 1688m, 1712m. Then what is 
+ *      the maximum length of a tape that would be able to measure all the 
+ *      sides exactly?
+ *      a) 12m  b) 4m   c) 6m  d) 2m
+ *  Ans:    2m
+ *      -   Difference of 1410 and 1456 is 46.
+ *      -   Factors of 46 are 23, 46 and 2.
+ *      -   Only 2 divide all the four numbers. Hence, option (d) is correct.
+ */
+
+/**
+ *  Side Of Largest Square
+ *  -   Find the side of the largest square slab which can be paved on the 
+ *      floor of a field 544m long and 374 m broad.
+ *      a) 56m  b) 42m   c) 38m  d) 34m
+ *  Ans:    34m
+ *      -   The side of the square slab is the H.C.F. of 544 and 374 i.e. 34.
+ *          Hence, option (d) is correct.
+ */
+
+/**
+ *  Milkman
+ *  -   A milkman has three kinds of varieties of milk. He has 506 liters, 536 
+ *      liters and 836 liters of the three varieties. What is the largest size 
+ *      of the bottle in which he can bottle each of the three types of milk 
+ *      completely without mixing the milk.
+ *      a) 12l  b) 4l   c) 6l  d) 2l
+ *  Ans:    2l
+ *      -   Required size of the bottle= HCF of (506, 536 and 836)
+ *      -   Difference of 506 and 536= 30.
+ *      -   Factors of 30 are 1, 2, 3, 5 , 6, 10, 15, 30.
+ *      -   Out of these factors only 2 divide all the three numbers (506, 536 
+ *          and 836). Hence, option (d) is correct.
+ */
+
+/**
+ *  Largest Number
+ *  -   Find the largest number which when divides 721 and 282, the respective 
+ *      remainders left are 1 and 6?
+ *      a) 12  b) 14   c) 20  d) 26
+ *  Ans:    12
+ *      -   HCF (721-1, 282-6) = HCF of (720, 276)=  12
+ *      -   So, the required answer is 12. Hence, option (a) is correct.
+ */
+
+/**
+ *  Largest Number And Remainder
+ *  -   Find the largest number which when divides 576, 876 and 1206, the 
+ *      remainders left are same.
+ *      a) 20  b) 30   c) 40  d) 50
+ *  Ans:    30  
+ *      -    Take the differences between any two pairs out of the given numbers.
+ *              1206 – 876 = 330
+ *              1206 – 576 = 630
+ *              876- 576= 300.
+ *      -   The required number is the HCF of these differences.
+ *      -   HCF of 300, 330 and 630 = 30
+ */
+
+/**
+ *  Greatest Number
+ *  -   Find the greatest number that will divide 131, 157 and 183 so as to 
+ *      leave the same remainder in each case
+ *      a) 13  b) 26   c) 36  d) 28
+ *  Ans:    26
+ *      -    Required number = H.C.F. of (183 - 131), (183 - 157) and 
+ *          (157 - 131) = H.C.F. of 52, 26 and 26 = 26
+ */ 
+
+/**
+ *      Formulae for 2 numbers only:
+ *          HCF * LCM = product of 2 numbers
+ */
+
+/**
+ *  LCM
+ *  -   Find the LCM of 2, 4, 8, 12.
+ *      a) 24  b) 16   c) 12  d) 48
+ *  Ans:    24
+ *      -   2 = 2
+ *          4 = 2^2
+ *          8 = 2^3
+ *          12 = 2^2 x 3
+ *          LCM = 2^3 x 3 = 24 
+ */
+
+/**
+ *  Predict the LCM?
+ *  -   Find the LCM of 17, 21, 245.
+ *      a)  17*3*5*7^2  b)  17*3*5  c)  17*3*7^2    d)  None of these
+ *  Ans:    17*3*5*7^2
+ *      -   17=17, 21= 3x 7, 245= 5x 7^2
+ *          Required LCM = 17 x 3 x 5 x 7^2
+ */
+
+/**
+ *  Find The LCM?
+ *  -   Find the LCM of 45, 90, 135.
+ *      a)  2^2*3^3*5   b) 2*3^3*5  c) 2*3^3*5^2    d)  None of these   
+ *  Ans:    2*3^3*5
+ *      -   45=5  x 3^2,
+ *          90= 3^2 x 2 x 5
+ *          135= 5 x 3^3
+ *          Required LCM = 2 X 3^3 X 5= 270. 
+ */
+
+/**
+ *  HCF & LCM
+ *  -   HCF of two expressions is (x + 1) and LCM is (x^3+ x^2 – x – 1). If 
+ *      one expression is (x^2 - 1), then what is the second expression?
+ *      a)  x+1     b)  (x-1)^2     c)  (x+1)^2     d)  (x-1)(x+1)
+ *  Ans:    (x+1)^2
+ *      -   HCF = (x + 1)
+ *          LCM = x^3 + x^2 – x – 1 = (x + 1) (x + 1) (x – 1)
+ *      -   First expression = x^2 – 1 = (x + 1) (x - 1).
+ *          Second expression × first expression = HCF x LCM
+ *          Second expression= HCF x LCM/first expression 
+ *          = ((x+1)(x+1)(x+1)(x-1))/((x-1)(x+1))= (x+1)^2
+ */
+
+/**
+ *  Two numbers:
+ *  -   The HCF and LCM of two numbers are 21 and 4641 respectively. If one     
+ *      of the numbers lies between 200 and 300, then the two numbers are:
+ *      a)  273, 357    b) 273, 361     c)  273, 359    d)  273, 363
+ *  Ans:    273, 357
+ *      -   HCF * LCM = Product of the numbers = 21 * 4641 =3*7*3*7*13*17.
+ *          The numbers can be 3 * 7 * 13 and 3 * 7 *17 ie, 273 and 357.
+ */
+
+/**
+ *  Bell
+ *  -   A bell rings every 18 minutes. A second bell rings every 24 minutes. 
+ *      A third bell rings every 32 minutes. If all the three bells ring at 
+ *      the same time at 8 o'clock in the morning, at what other time will 
+ *      they all ring together?
+ *      a)  12:40pm     b) 12:48pm  c) 12:56pm  d)  13:04pm
+ *  Ans:    12:48pm
+ *      -   LCM of 18, 24 and 32 is 288
+ *      -   288 minutes = 4 hours 48 minutes.       
+ *      -   The bells will ring together again at 8 AM+ 4 hours 48 
+ *          minutes= 12:48 PM.
+ */ 

e1c83be471ca28a2c4fcb2203aae2b7bd263657d
“tarunve” <tarun.verma151@gmail.com>
2021-11-27 21:41:36 +0530

    aptitude notes

---
+++ 04-aptitude/modules/002-Averages.cpp
@@ -0,0 +1,188 @@
+/**
+ *  -   Average = (sum of numbers)/(no. of numbers)
+ *  
+ *  3 measures of central tendencies
+ *  -   Mean :  Average
+ *  -   Median : Central number between numbers. Mainly in even distributed 
+ *          numbers but not in skewed numbers.
+ *  -   Mode :  Maximum occuring value in group of numbers. 
+ * 
+ *  Average of weighted groups
+ *      W(x) = (n1*w1 + n2w2 + n3*w3 + ..... ) / (n1+n2+n3+....)
+ */
+
+/**
+ *  Average Of Odd Numbers
+ *  -   The average of all odd numbers between 20 and 30 is:
+ *      a) 23   b) 21   c) 25   d) 27
+ *  Ans:    25
+ *      -   Average of all odd numbers between 20 and 30 =(21+23+25+27+29)/5
+ *          =125/5 = 25.
+ */
+
+/**
+ *  Average Of Multiples
+ *  -   Average of first 20 multiples of 4 is:
+ *      a) 40   b) 42   c) 44   d) 48
+ *  Ans:    42
+ */
+
+/**
+ *  Average Of Numbers
+ *  -   The average of the following numbers: 1001, 1002, 1003, 1006, 1009
+ *      a) 1004 b)  1004.2  c) 1004.8   d) 1004.1
+ *  Ans:    1004.2
+ *      -   lets the average of the numbers is 1005.
+ *      -   Net average difference= -4/5= -0.8
+ *      -   Hence, the required average= 1005- 0.8= 1004.2
+ */
+
+/**
+ *  Average Of Numbers - 2
+ *  -   The average of the following numbers: 1911, 1922, 1915, 1916, 1920
+ *      a) 1916     b) 1916.4   c) 1916.8   d) 1917
+ *  Ans:    1916.8
+ *      -   lets the average of the numbers is 1915. Net average 
+ *          difference= 9/5= +1.8
+ *          Hence, the required average= 1915+ 1.8= 1916.8
+ */
+
+/**
+ *  Sachin And Average
+ *  -   Sachin has an average of 90 after ten innings. If he scored 50 runs in 
+ *      the 11th inning then what is the average of Sachin after 11 innings?
+ *      a) 86.36    b) 85.47    c) 87.87    d) None of These
+ *  Ans:    86.36
+ *      -   Average runs scored after 11th innings= (90×10+50)/(10+1)=950/11
+ *          =86.36. Hence, option (a) is correct.
+ */
+
+/**
+ *  Manish And Average
+ *  -   Manish average score in first five tests was 10. If after 6th test he 
+ *      scored 12 marks then what was his average after 6 tests?
+ *      a) 10.36    b) 10.33    c) 17.87    d) None of these
+ *  Ans:    10.33
+ *      -   Average score of Manish after six tests= (10×5+12)/(6)=62/6=10.33.  
+ *          Hence, option (b) is correct.
+ */
+
+/**
+ *  Average Of Prime Numbers
+ *  -   The average of five distinct prime numbers is 6 if first four of them 
+ *      are one digit prime numbers then value of 5th prime number is:
+ *      a) 11   b) 13   c) 17   d) 19
+ *  Ans: 13
+ *      -   One digit prime numbers are 2, 3, 5, 7. Let the 5th prime numbers 
+ *          is x then according to the question: (2+ 3+ 5+ 7+ x)/5 = 6 
+ */
+
+/**
+ *  Student Weight
+ *  -   When a student weighing 46 kgs left a class, the average weight of the 
+ *      remaining 59 students increased by 300g. What is the average weight of  
+ *      the remaining 59 students?
+ *      a) 88   b) 98   c) 64   d) 57
+ *  Ans: 64 
+ *      -   Let the average weight of the 59 students be A.
+ *      -   Therefore, the total weight of the 59 of them will be 59A.
+ *      -   The questions states that when the weight of this student who left 
+ *          is added, the total weight of the class = 59A + 46
+ *      -   When this student is also included, the average weight decreases 
+ *          by 0.3 kgs.
+ *          ( 59A + 46)/ 60 = A – 0.3
+ *          => 59A + 46 = 60A - 18
+ *          => 46 + 18 = 60A - 59A
+ *          => A = 64 
+ */
+
+/**
+ *  Average Of Men Weight
+ *  -   The average weight of 8 men on a rope bridge is increased by 2.5 kg, 
+ *      when one of the men, who weighs 69 kg, is replaced by a new man. Find 
+ *      the weight of the new man.
+ *      a) 79   b) 89   c) 84   d) 69
+ *  Ans: 89
+ *      -   Let average weight of seven men be x years.
+ *          (7x+69)/8=A
+ *          (7x+N)/8=A+2.5 ,where N is weight of new man.
+ *          Solving we get N=89 kg.
+ */
+
+/**
+ *  Average Honey
+ *  -   The average honey collected by 19 bees of a beehive is 50 grams per 
+ *      day. If the queen bee’s collection is added, the average collection 
+ *      becomes 47.5 grams. What is the queen bee’s collection?
+ *      a) 4 grams  b) 0 grams  c) 6 grams  d) None of these.
+ *  Ans: 0 grams
+ *      -   Let queen bee’s collection be q grams;
+ *          ((19×50)+q)/20=47.5;
+ *          Solving we get q=0
+ */
+
+/**
+ *  Rahul And Average
+ *  -   The average score of Rahul after 20 innings is 20 and in the 21st 
+ *      innings Rahul scores 83 runs. In the 22nd innings the minimum number 
+ *      of runs required to increase his average score by 2 than it was before 
+ *      the 22nd innings :
+ *      a) 66   b) 68   c) 67   d) 69
+ *  Ans: 67
+ *      -   The average score after 20th innings = 20 
+ *          The average score after 21st innings = 23
+ *          Since [(20 × 20) +83]/21 = 483/21 = 23
+ *          Now requirement of runs = 25 + (21 × 2) = 67
+ *          Or   (25 × 22) – (23 × 21) = 67 
+ */
+
+/**
+ *  Persons And Hotels
+ *  -   Nine persons went to a hotel for taking their meals. Eight of them 
+ *      spent Rs.15 each on their meals and the ninth spent Rs.8 more than the 
+ *      average expenditure of all the nine. What was the total money spent by 
+ *      them.
+ *      a) 130  b) 131 c) 132   d) 144
+ *  Ans:    144
+ *      -   Let the average expenditure of all the nine be RS.x
+ *          Then, 15 * 8 + (x + 8) = 9x
+ *          Therefore x = 16
+ *          Total money spent = 9x = RS.(9 * 16) = Rs.144
+ */
+
+/**
+ *  Student & Sections
+ *  -   There are two sections A and B of a class, consisting of 40 and 48 
+ *      students’ respectively. If the average weight of section A is 50kg and 
+ *      that of section B is 45kg, find the average weight of the whole class.
+ *      a) 45.25    b) 46.26    c) 47.27    d) 48.28
+ *  Ans:    47.27
+ *      -   Total weight of (40+48) Students = (40x50+48x45)kg = 4160kg
+ *          Therefore average weight of the whole class = (4160/88) kg
+ *          Therefore average weight = 47.27kg
+ */
+
+/**
+ *  Students And Average
+ *  -   In a class twenty five students are standing in a row the average 
+ *      weight of 1st 12 students is 14 kg. that of last twelve is 17 kg. 
+ *      If the average weight of all the 25 students is 18 kg. then find 
+ *      the weight of 13th student.
+ *      a) 76   b) 77   c) 78   d) 80 kg
+ *  Ans:    78
+ *      -   Let the weight of 13th student be w kg.
+ *          Total weight of first 12 students = 12×14=168kg.
+ *          Total weight of last 12 students = 12×17=204 kg
+ *          Total weight of all the students = 168 + w + 204 = w + 372 = 25×18=450
+ *          w = 450- 372 = 78 kg.
+ */
+
+/**
+ *  Average Weight Of Students
+ *  -   The average weight of 35 students in a school 40 kg. If 5 students of 
+ *      average weight 100 kg. left the school then the new average of remaining 
+ *      students is:
+ *      a) 35 kg    b) 30 kg    c) 40 kg    d) 25 kg
+ *  Ans: 30 kg
+ *      -   (35*40 - 5*100) /30 = 30kg
+ */

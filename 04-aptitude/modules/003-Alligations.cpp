79ae08d21565c27df6ea43b04d98d2573ba432b1
“tarunve” <tarun.verma151@gmail.com>
2021-11-27 21:41:40 +0530

    aptitude notes

---
+++ 04-aptitude/modules/003-Alligations.cpp
@@ -0,0 +1,121 @@
+/**
+ *  Alligations:
+ *  -   visual approach to solve weighted average problems involving mixing of
+ *      2 groups.
+ *  -       n1/n2 = (A2-Aw)/(Aw-A1)
+ */
+
+/**
+ *  Man And Rice
+ *  -   The ratio in which a man must mix rice at 10.20 per kg and 14.40 per 
+ *      kg so as to make a mixture worth 12.60 per kg, is
+ *      a)  4:3     b) 2:5  c) 3:4  d) None
+ *  Ans:    3:4
+ */
+
+/**
+ *  Sugar And Salt
+ *  -   If 10 kg of sugar costing Rs.15/kg and 20 kg of salt costing Rs. 10/kg 
+ *      are mixed, find the average cost of the mixture Rs. per kilogram.
+ *      a)  11:33   b) 11.67    c) 12:33 d) 12.67
+ *  Ans: 11:67
+ *      -   (15*10 + 20*10)/(10+20) = 11.67
+ */
+
+/**
+ *  Tea And Mixture
+ *  -   In what ratio should two qualities of tea having the rates of Rs. 40 per 
+ *      kg and Rs. 30 per kg be mixed in order to get a mixture that would have 
+ *      a rate of Rs 35 per kg?
+ *      a) 1:1  b) 1:2  c) 1:3  d) 3:1
+ *  Ans: 1:1
+ *      -   30------40 , diff is 10. for 35 as the average , it will divide in
+ *          1:1 i.e. 30-35 , 35-40.
+ *      -   another way of looking at it :  (1*30 + 1*40)/2 = 35
+ */
+
+/**
+ *  Table And Chair
+ *  -   A man purchased a table and a chair for Rs.2000. He sold the table at 
+ *      a profit of 20% and the chair at a profit of 40%. In this way, his 
+ *      total profit was 25%. Find the cost price (in Rs.) of the table.
+ *      a) 1500     b) 900  c) 800  d) 1000
+ *  Ans:    1500
+ */
+
+/**
+ *  Milk And Mixture
+ *  -   Two types of milk having the rates of Rs.8/kg and Rs.10/kg respectively 
+ *      are mixed in order to produce a mixture having the rate of Rs.9.20/kg. 
+ *      What should be the amount of the second type of milk if the amount of 
+ *      the first type of milk in the mixture is 20 kg?
+ *      a) 20Kg     b) 25kg    c) 30kg    d) 40kg
+ *  Ans: 30Kg
+ *      -   160+ 10y = 9.2y + 184
+ *          0.8y = 24
+ *          y = 30
+ */
+
+/**
+ *  Ashok And Grains
+ *  -   Ashok purchased two qualities of grains at the rate of Rs. 100 per 
+ *      quintal and Rs. 160 per quintal. In 50 quintals of the second quality, 
+ *      how much pulse of the first quality should be mixed so that by selling 
+ *      the resulting mixture at Rs. 195 per quintal, he gains a profit of 30%?
+ *      a) 10 quintals  b) 14 quintals  c) 20 quintals d) None
+ *  Ans: 10 quintals
+ */
+
+/**
+ *  Milk And Water
+ *  -   There are two mixtures of milk and water, the quantity of milk in them 
+ *      being 20% and 80% of the mixture. If 2 liters of the first are mixed 
+ *      with three liters of the second, what will be the ratio of milk to 
+ *      water in the new mixture?
+ *      a) 11:9     b) 11:1     c) 19:11    d) 14:11
+ *  Ans: 14:11
+ */     
+
+/**
+ *  Darjeeling And Assam Tea
+ *  -   The ratio in which the Darjeeling tea at Rs. 32per kg is mixed with the 
+ *      Assam tea at Rs. 25 per kg so as to gain 20% by selling the mixture at 
+ *      Rs.32.40 per kg is
+ *      a) 4:5  b) 2:5  c) 5:2  d) 2:3
+ *  Ans: 2:5
+ */
+
+/**
+ *  Varieties of Rice
+ *  -   In what ratio must a merchant mix two varieties of rice costing Rs. 15 
+ *      and Rs.20 per kg respectively so as to get a mixture worth Rs. 16.50 
+ *      per kg?
+ *      a) 3:5  b) 5:3 c) 7:3   d) 7:6
+ *  Ans: 7:3
+ */
+
+/**
+ *  Petrol and Water
+ *  -   40litres of a mixture of petrol and water contains 10% of water. The 
+ *      water to be added, to make the water content 20% in the new mixture is:
+ *      a) 4 litres    b) 6 litres  c) 5.5 litres   d) 5 litres
+ *  Ans: 5 litres
+ */
+
+/**
+ *  Type1 and Type 2 Rice
+ *  -   The cost of Type 1 rice is Rs. 15 per kg and Type 2 rice is Rs. 20 per 
+ *      kg. If both Type 1 and Type 2 are mixed in the ratio of 1: 4, then the 
+ *      price per kg of the mixed variety of rice is:
+ *      a) 18   b) 19   c) 23   d) 61
+ * Ans: 19
+ */
+
+/**
+ *  Merchant And Sugar
+ *  -   A merchant has 1000 kg of sugar, part of which he sells at 8% profit 
+ *      and the rest at 16% profit. He gains 14% on the whole. The quantity 
+ *      sold at 16% profit is:
+ *      a) 200  b) 400  c) 750   d) 920
+ *  Ans: 750
+ */ 

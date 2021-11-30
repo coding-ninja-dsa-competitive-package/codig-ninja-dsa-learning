eea6b7717a4cbc78918f86288d55bd6503e7855b
“tarunve” <tarun.verma151@gmail.com>
2021-11-30 15:40:27 +0530

    aptitude problems

---
+++ 04-aptitude/modules/005-Ratio-Proportaion-Variation-1.cpp
@@ -0,0 +1,120 @@
+/**
+ *  Find the ratio?
+ *  -   120/180 = ?
+ *  Ans: 2/3
+ */
+
+/**
+ *  Find the value?
+ *  -   What is the value of 2: 5?
+ *  Ans:    0.4
+ */
+
+/**
+ *  Value of a
+ *  -   What is the value of (1+a)/3 if a= 0.5? 
+ *  Ans:    1/2
+ *      -      (1+ a)/3= (1+0.5)/3= 1.5/3= ½
+ */
+
+/**
+ *  Judge it?
+ *  -   (400+2)/(800+2) is ……than 400/800.
+ *      a) greater  b) less than    c) equal to     d) can't be determined
+ *  Ans:    greater
+ */
+
+/**
+ *  Judge It Again?
+ *  -   1/2 ? ((1+100)/(2+200)), what should come in place of ?
+ *      a) >    b) <    c) =    d) None 
+ *  Ans: =
+ */ 
+
+/**
+ *  What is x, y and z?
+ *  -   If x: y = 3 : 2 and y: z = 6: 5 then x: y: z =?
+ *      a) 9:6:4    b) 9:6:5    c) 3:2:0.5  d) 3:6:5
+ *  Ans:    9:6:5
+ *      -   x: y = 3: 2, y: z = 6: 5
+ *          x: y: z = (3*6): (2*6): (2*5) = 18: 12: 10 = 9: 6: 5
+ */
+
+/**
+ *  Find a:c?
+ *  -   If a: b = 1: 2 & b: c= 3: 4 then a: c = ?
+ *      a) 3:6  b) 3:4  c) 1:4  d) 3:8
+ *  Ans: 3:8
+ *      -    a: b = 1: 2 & b: c= 3: 4 then a: b: c = 1×3:2×3:2×4= 3:6:8
+ */
+
+/**
+ *  Find x:t?
+ *  -   If x: y = 1: 2, y: z = 3: 4, z: w= 5: 6, w: t = 7: 8 then x: t = ?
+ *      a) 1:8  b) 105:336  c) 35:128 d) None of these
+ *  Ans:    35:128
+ *      -   If x: y = 1: 2, y: z = 3: 4, z: w= 5: 6, w: t = 7: 8
+ *          Then x: t = 1×3×5×7:2×4×6×8=105:384=35:128
+ */
+
+/**
+ *  Denominations
+ *  -   A boy has a few coins of denominations 50 paise, 25 paise and 10 paise 
+ *      in the ratio 1 : 2 : 3. If the total amount of the coins is Rs. 6.50, 
+ *      the number of 10 paise coins is
+ *      a) 5    b)  10      c) 15   d) 20
+ *  Ans:    15
+ *      -   Let the numbers of 50 paise, 25 paise and 10 paise are x, 2x and 3x.
+ *          50x+25×2x+10×3x=650 
+ *          130x=650 
+ *          x=5 
+ *          2x=10,3x=15 
+ *          Number of 10 paise= 15. Hence, option (c) is correct. 
+ */
+
+/**
+ *  Boy And Denominations
+ *  -   A boy has a few coins of denominations 10 paise, 20 paise and 25 paise 
+ *      in the ratio 10 : 15 : 6. Then, the possible value of the denomination=?
+ *      a) 5    b) 11   c) 15   d)  20 
+ *  Ans: 11
+ *      -   Let the possible number of coins be 10x, 15x and 6x.
+ *          0.1×10x+0.20×15x+0.25×6x=5.5x. 
+ *          Option (b) is the multiple of 5.5.
+ *          Hence, only option (b) is correct. 
+ */
+
+/**
+ *  Divide Among Workers
+ *  -   Rs.432 is divided amongst three workers A, B and C such that 8 times A’s 
+ *      share is equal to 12 times B’s share which is equal to 6 times C’s share. 
+ *      How much did B get?
+ *      a) 24   b) 48   c) 192  d) 96
+ *  Ans:    96
+ *      -   8 times A’s share = 12 times B’s share = 6 times C’s share
+ *      -   8a= 12b= 6c (where a, b and c are shares of A,B and C)
+ *      -   Note that this is not the same as the ratio of their wages being 
+ *          8:12:6. In this case, find out the L.C.M of 8, 12 and 6 and divide 
+ *          the L.C.M by each of the above numbers to get the ratio of their 
+ *          respective shares.
+ *      -   The L.C.M of 8, 12 and 6 is 24.
+ *      -   Therefore, the ratio A:B:C:: (24/8): (24/12) : (24/6):: 3:2:4;
+ *      -   The sum of the total wages=3x+2x+4x=432 => 9x=432 or x=48.
+ *          Hence B gets 2 X 48= Rs. 96
+ */
+
+/**
+ *  Proportion-1
+ *  -   If 3: 2 :: x: 26, then x= ?
+ *      a) 39   b) 13   c) 26   d) None of these
+ *  Ans:    39
+ *      -   3/2=x/26 or x= 39. Hence, option (a) is correct.
+ */
+
+/**
+ *  Find b?
+ *  -   If 2: b:: b: 32, then b= ?(It should be positive)
+ *      a) 8    b) 4    c) 16   d) 2
+ *  Ans:    8
+ *      -   2/b=b/32 or b^2=64 or b=8. Hence, option (a) is correct.
+ */

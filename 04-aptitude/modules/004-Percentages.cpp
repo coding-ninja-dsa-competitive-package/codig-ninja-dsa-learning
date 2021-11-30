f1f94cadda9f7f5cb23330723995b66b4d451691
“tarunve” <tarun.verma151@gmail.com>
2021-11-30 15:40:23 +0530

    aptitude problems

---
+++ 04-aptitude/modules/004-Percentages.cpp
@@ -0,0 +1,183 @@
+/**
+ *  Product Constancy Table:
+ *  -   If any component is increasing by (x/a)% then second component will decrease
+ *      by (x/a+x)% to keep the result constant.
+ */
+
+/**
+ *  Calculate Percentage
+ *  -   24% =  6/25
+ */
+
+/**
+ *  In Terms Of Percentage
+ *  -   3/7= ?(In Terms Of Percentage)(Calculate the approx. value)
+ *      a) 34%  b) 43%  c) 28%  d) None
+ *  Ans: 43%
+ *      -   3/7= 3/7×100=300/7=42.9%≈43%
+ */
+
+/**
+ *  Simplify Percent
+ *  -   30% = 
+ *      a) 3/25     b) 7/50     c) 1/5   d) None
+ *  Ans:    None
+ *      -   30%= 30/100= 3/10. Hence, option (d) is correct.
+ */ 
+
+/**
+ *  Ramesh Speed
+ *  -   Ramesh initial speed was 50 kmph if he increased his speed by 20% then 
+ *      what would be his new speed?
+ *      a) 60km/h   b) 70km/h   c) 55km/h   d) None
+ *  Ans: 60km/h
+ *      -   Final speed= 50+ 20% of 50= 50+ (20/100)×50=50+10=60 kmph
+ */
+
+/**
+ *  Sahir's Wealth
+ *  -   Sahir’s wealth at the start of the year was Rs. 1000 and at the end 
+ *      of the year it was Rs. 1500. What was the percentage increment in the 
+ *      wealth of Sahir at the end of the year from the start of the year?
+ *      a) 45%  b) 50%  c) 75%  d) None of these
+ *  Ans:    None of these
+ *      -   Percentage increment in wealth= ((1500-1000)/1000)×100=50%
+ *          Hence, option (b) is correct.
+ */
+
+/**
+ *  Ram And His Speed
+ *  -   Ram’s initial speed was 5 kmph and if he increased his speed by 14% 
+ *      then what would be his final speed?
+ *      a) 5.7 km/h     b) 4.5 km/h     c) 5.6 km/h     d) None
+ *  Ans: 5.7 km/h
+ *      -   10% of 5= 0.5 , 4% of 5= 0.2
+ *          Final speed= 5+ 0.5+ 0.2= 5.7 kmph. Hence, option (a) is correct.
+ */
+
+/**
+ *  Percentage Change
+ *  -   If P=XY,X was increased by 30% and Y was increased by 20% then what 
+ *      is the percentage change in P?
+ *      a) 50%  b) 60%  c) 56%  d) None of these
+ *  Ans: 56% 
+ *      -   Final value of P= 1.3X*1.2Y= 1.56XY
+ *          Hence, the percentage change = 56%. Option (c) is correct.
+ */
+
+/**
+ *  Calculate Value Of C?
+ *  -   120 (Increment by 10%) -> A (Increment by 20%) -> B (Increment by 30%) 
+ *      -> C.
+ *      What is the value of C?
+ *      a) 205.92   b) 160.05   c) 132.05   d) None of these
+ *  Ans: 205.92
+ */
+
+/**
+ *  Change in Salary
+ *  -   A’s salary is increased by 10% and then decreased by 10%. The change 
+ *      in salary is
+ *      a) 0%   b) 1% decrease  c) 1% increase  d) 2% decrease
+ *  Ans:    1% decrease
+ *      -   Change in salary = (10-10+((10 ×-10)/100)% = -1
+ *          =   -((10×10)/100)=-1%
+ *      -   Negative sign shows decrease in salary. Hence, option (b) is correct.
+ */
+
+/**
+ *  Height Of Triangle
+ *  -   The height of a triangle is increased by 20%. What can be the maximum 
+ *      percentage increase in length of the base so that the increase in area 
+ *      is restricted to a maximum of 50%?
+ *      a) 50%  b) 25%  c) 14.28%   d) 35%
+ *  Ans:    25%
+ */
+
+/**
+ *  Price Of Sugar
+ *  -   The price of sugar falls by 20%. How much sugar can be bought now with 
+ *      the money that was sufficient to buy 20kg of sugar previously?
+ *      a) 5kg  b) 15kg c) 25kg     d) 30kg
+ *  Ans:    25kg    
+ */ 
+
+/**
+ *  Rajeev And Goods
+ *  -   Rajeev buys good worth Rs. 6000. He gets a rebate of 10% on it. After 
+ *      getting the rebate, he pays sales tax @ 20%. Find the amount he will 
+ *      have to pay for the goods?
+ *      a) 6400     b) 6480     c) 6520     d) 6690
+ *  Ans:    6480
+ *      -   Rebate = 10% of 6000 =600;
+ *      -   Sales tax = 20% of (6000-600) =1080;
+ *      -   Amount to be paid = 5400 + 1080 = 6480
+ */ 
+
+/**
+ *  Price Of Article    
+ *  -   The price of an article is first decreased by 20% and then increased 
+ *      by 30%. If the resulting price is Rs 416, the original price of the 
+ *      article is
+ *      a) Rs 350   b) Rs 405   c) Rs 400   d)  Rs 450
+ *  Ans:   Rs 400
+ */
+
+/**
+ *  Volume Of Box
+ *  -   The length, breadth and height of an box in the shape of a cuboid are 
+ *      decreased by 10%, 20% and 10% respectively. Find the % change in the 
+ *      volume of the box.
+ *      a) 35.2%    b) -35.2%   c) 34%  d) 33.33%
+ *  Ans: -35.2%
+ *      -   Let the original length, breadth and height be L,B,H respectively
+ *          New dimensions=0.9L, 0.8B, 0.9H
+ *          New volume=0.648(LBH)
+ *          % decrease=[(LBH-0.648LBH)/LBH]X100%=35.2%
+ *          %change= -35.2%. Negative sign denotes decrease
+ */
+
+/**
+ *  Rectangle Volume
+ *  -   The length and breadth of a rectangle are changed by +10% and -10%. 
+ *      What is the percentage change in area of rectangle?
+ *      a) -1%  b) -2%  c) +1%  d) +2%
+ *  Ans:    -1%
+ *      -    Let the initial length and breadth ‘l’ and ‘b’. The area of 
+ *          rectangle = lb
+ *      -   Final length and breadth of rectangle 1.1l and 0.9b. 
+ *      -   The area of rectangle= 1.1l×0.9b=0.99lb
+ *      -   % change in area = ((0.99lb-lb)/lb)×100=-1%
+ */
+
+/**
+ *  Hike In Salary
+ *  -   I recently got a promotion accompanied by 23% hike in salary but due 
+ *      to recession it my salary was reduced by 32%.What was net change in 
+ *      my salary?
+ *      a) 15.89% decrement     b) 15.89% increment c) 16.4% increment
+ *      d)  16.4% decrement
+ *  Ans: 16.4% decrement
+ *      -   Let initially salary be 100
+ *          After hike salary = 123
+ *          After decrement salary = 83.64
+ *          Net change= (83.64-100)/100=16.4 decrement 
+ */
+
+/**
+ *  Cost Of Article
+ *  -   The cost of an article worth Rs100 is increased by 25% first and again 
+ *      decreased by x%. The final cost of the article is Rs. 100. The value of 
+ *      x is
+ *      a) 20   b) 21   c) 110  d) 121
+ * Ans:     20
+ */
+
+/**
+ *  Cost Of Articles - 2
+ *  -   The cost of an article worth Rs 1000 is increased by 20% first and 
+ *      again decreased by x%. The final cost of the article is Rs. 1000. 
+ *      The value of x is
+ *      a) 20   b) 21   c) 110  d) 16.67
+ *  Ans: 16.67
+ */

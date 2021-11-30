01eafff5a2baab08adf9eda7300f50849a6b5270
“tarunve” <tarun.verma151@gmail.com>
2021-11-30 15:40:31 +0530

    aptitude problems

---
+++ 04-aptitude/modules/006-Profit-And-Loss.cpp
@@ -0,0 +1,244 @@
+/**
+ *  Simple Interest:
+ *  -   Interest = (P*R*T)/100      -- Principal, Rate of interest , time
+ *  -   Number of Time Periods = Total Interest / Annual Interest
+ *  
+ *  Compound Interest:
+ *  -   A = P*(1+ R/100)^T
+ */
+
+/**
+ *  Man And Cycle
+ *  -   A man buys a cycle for Rs. 1400 and sells it at a loss of 15%. What 
+ *      is the selling price (in Rs.) of the cycle?
+ *      a) 1202     b) 1190     c) 1160     d) 1000
+ *  Ans:    1190
+ *      -   Selling price= 
+ *      -   Cost price – Loss= 1400-1400×(15/100)=1400-210=Rs.1190.
+ *          Hence, option (2) is correct.
+ */ 
+
+/**
+ *  Retailer And Radio
+ *  -   A retailer buys a radio for Rs. 225. His overhead expenses are Rs.15. 
+ *      He sells the radio for Rs. 300. The profit per cent of the retailer 
+ *      is :
+ *      a) 25%  b) 26.67%   c) 20%  d) 33.33%
+ *  Ans:    25%
+ *      -   Total cost price= Rs. 225+ Rs. 15= Rs. 240.
+ *      -   Percentage profit= (60/240)×100=25% profit. Hence, option (1) is 
+ *          correct.
+ */
+
+/**
+ *  Tradesman And Goods
+ *  -   A tradesman marks his goods 10% above his cost price. If he allows 
+ *      his customers 10% discount on the marked price, how much profit or 
+ *      loss does he make, if any?
+ *      a) 1% gain  b) 1% loss c) 5% gain d) No gain, No loss
+ *  Ans:    1% loss
+ *      -   Let the cost price is Rs. 100.
+ *          Marked price= Rs. 100+ 10% of 100= Rs. 110.
+ *          Selling price= 110- 10% of 110= Rs.99.
+ *          Net loss= 100- 99= Rs. 1.
+ *          Percentage loss= (1/100)×100=1% loss. Hence, option (2) is correct.
+ */
+
+/**
+ *  Trade And Goods
+ *  -   A trader marks his goods 45% above the cost price and gives a discount 
+ *      of 20% on the marked price. The gain % on goods he makes is :
+ *      a) 15%  b) 14%  c) 29%  d) 16%  
+ *  Ans:     16%
+ *      -   Let the cost price= Rs. 100.
+ *      -   Marked price= 100+ 45% of 100= Rs. 145.
+ *      -   Selling price= 145- 20% of 145= Rs. 116.    
+ *      -   Percentage gain= (116-100)/100×100=16%. Hence, option (4) is correct.
+ */
+
+/**
+ *  Shopkeeper And Chair
+ *  -   A shopkeeper purchased a chair marked at Rs.800, at two successive 
+ *      discounts of 10% and 15% respectively. He spent Rs.28 on transportation 
+ *      and sold the chair for Rs.800. His gain percent is :
+ *      a)  40%     b) 30%  c) 25%  d) 14%
+ *  Ans: 25%
+ *      -   Two successive discounts of 10% and 15% is equivalent to 
+ *          10+15-((10×15)/100)=23.5%.
+ *      -   After two successive discounts of 10% and 15% shopkeeper get the 
+ *          chair at 800- 23.5% of 800= Rs. 612.
+ *      -   But he had to pay Rs. 28 on transportation. Hence, the net cost 
+ *          price=612+ 28= Rs. 640.
+ *      -   Selling price= Rs. 800
+ *      -   Net profit= 800- 640= Rs. 160.
+ *      -   Percentage profit= (160/640)×100=25%. Hence, option (3) is correct.
+ */
+
+/**
+ *  Better to Customer
+ *  -   Which of the following successive discounts is better to a customer?
+ *      (a) 20%, 15%, 10% or
+ *      (b) 25%, 12%, 8% ?
+ *      a) (a) is better    b) (b) is better    c) (a) or (b) are same
+ *      d) None of these
+ *  Ans:    (b) is better
+ *      -   Let’s check both the given discount schemes one by one:
+ *      -   (a) 20%, 15% and 10%.
+ *              If the marked price is Rs. 100----(-20%)---->80----(-15%)
+ *              ------>68----(-10%)---->61.20
+ *              Net discount= 100- 61.20= Rs.38.80.
+ *          (b) 25%, 12%, 8% 
+ *              If the marked price is Rs. 100----(-25%)---->75----(-12%)
+ *              ------>66----(-8%)---->60.72
+ *              Net discount= 100- 60.72= Rs.39.28.
+ *      -   (b) gives a better discount than (a) hence, b is better. Hence, 
+ *          option (2) is correct.
+ */
+
+/**
+ *  Interest per Annum
+ *  -   In what time will Rs. 500 give Rs. 50 as interest at the rate of 5% 
+ *      per annum?
+ *      a)  2 years     b) 3 years  c) 4 years  d) 5 years  
+ *  Ans:    2 years
+ *      -   50 = ((500*5*t)/100)=>t=2 years
+ */
+
+/**
+ *  Rate Of Interest
+ *  -   A sum of 1600 gives a simple interest of 252 in 2 years and 3 months. 
+ *      The rate of interest per annum is:
+ *      a) 11/2%    b) 8%   c) 7%   d) 6%
+ *  Ans: 7%
+ *      -   Interest per annum= 252/2.25= Rs.112
+ *      -   Interest rate= (112/1600)*100=7% per annum. 
+ *      -   Hence, option (c) is correct.
+ */
+
+/**
+ *  Calculate R.O.I?
+ *  -   A sum of Rs.10,500 yield interest of Rs.3150 in 5 years at the rate of 
+ *      simple interest. What is the rate of interest?
+ *      a) 4%   b) 5%   c) 6%   d) None of these
+ *  Ans:    6%
+ *      -   Required rate of interest= (3150×100)/(10500×5)=6.0% per annum. 
+ *          Hence, option d is correct.
+ */
+
+/**
+ *  Ashok And Loan
+ *  -   Ashok took a loan of Rs.10000 with simple interest for as many years 
+ *      as the rate of interest. If she paid Rs.625 as interest at the end of 
+ *      the loan period, what was the rate of interest?
+ *      a) 2    b) 2.5  c) 5    d) Cannot be determined
+ *  Ans:    2.5
+ *      -   Let the interest rate and the time be x% per annum and x years 
+ *          respectively.
+ *              (10000*x*x)/100=625
+ *              x^2=(625/100) or x=25/10=2.5% per annum.
+ */
+
+/**
+ *  Total Sum
+ *  -   A sum fetched a total simple interest of Rs.420 at the rate of 3% per 
+ *      annum in 14 years. What is the sum?
+ *      a) Rs 1000  b) Rs 800   c) Rs 900   d) None of these
+ *  Ans:    Rs 1000
+ *      -   Let the sum be Rs. x.
+ *          Interest for the sum for 14 years= Rs. 420
+ *          x*(3/100)×14=420
+ *          0.42x=420
+ *          x=Rs. 1000
+ */
+
+/**
+ *  Amount Increase
+ *  -   X lent Rs. 1000 to Y for 3 years and Rs. 2000 to Z for 6 years on 
+ *      simple interest at the same rate of interest and received Rs.2250 in 
+ *      all from both of them as interest. The rate of interest per annum is :
+ *      a)  12.5%   b) 15%  c) 17.5%    d) None of these
+ *  Ans: 15%    
+ *      -   Let the rate of interest be r% per annum. According to the question:
+ *          1000×(r/100)×3+2000×(r/100)×6=2250
+ *          By solving the above equation we get: r= 15% per annum.
+ */
+
+/**
+ *  Calculate Principal?
+ *  -   The principal, which will amount to Rs. 270.40 in 2 years at the rate 
+ *      of 4% per annum compound interest, is (in Rs.)
+ *      a) 200  b) 225  c) 250  d) 220
+ *  Ans:    250
+ */
+
+/**
+ *  Calculate CI?
+ *  -   The compound interest (in Rs.) on Rs. 2000 in 2 years if the rate of 
+ *      interest is 4% per annum for the first year and 3% per annum for the 
+ *      second year, will be
+ *      a) 142.40   b) 140.40   c) 141.40   d) 143.40
+ *  Ans:    142.40
+ *      -   Principal amount= Rs. 2000
+ *      -   Amount after 1 year= 2000+ 4% of Rs. 2000= Rs. 2000+ 80
+ *      -   Amount after 2 years= Rs. 2000+ 80+ 3% of Rs. 2000+ 80= 
+ *          Rs. 2000+ 80+ 60+ 2.4=  Rs. 2142.40
+ *      -   Required interest= Rs. 2142.40- 2000= Rs. 142.40. Hence, 
+ *          option (a) is correct.
+ */
+
+/**
+ *  Find CI?
+ *  -   Find the compound interest on Rs. 5000 at a rate of 10% per annum 
+ *      for 1.5 years, compounded half yearly.
+ *      a) Rs 750   b) Rs 788.125   c) rs 780   d)  None of these
+ *  Ans:     Rs 788.125
+ *      -   Interest rate= 10% per annum or 5% per half year.
+ *      -   Time= 3 half years
+ *      -   Total amount after 1.5 years=5000[1+(5/100)]^3=Rs.5788.125
+ *      -   C.I. = Rs. (5788.125- 5000)= Rs. 788.125. Hence, option b is correct.
+ */
+
+/**
+ *  Compounded Quarterly
+ *  -   Find the compound interest on Rs. 8000 at a rate of 20% per annum for 
+ *      9months, compounded quarterly.
+ *      a) Rs 1261  b) Rs 1200  c) Rs 1161  d) None of these
+ *  Ans:    Rs 1261
+ *      -    Interest rate= 20% per annum or 5% per quarter.
+ *      -   Time= 3 quarters.
+ *      -   Total amount after 1.5 years= 8000[1+(5/100)]^3=
+ *          8000×((21/20) ×(21/20)x(21/20))=Rs. 9261
+ *      -   Compound interest = Rs. (9261- 8000)= Rs. 1261. Hence, option a 
+ *          is correct.
+ */
+
+/**
+ *  Sum Of Money
+ *  -   The difference between simple interest and compound interest of a 
+ *      certain sum of money at 20% per annum for 2 years is Rs. 48. Then 
+ *      the sum is (in Rs.)
+ *      a) 1000     b) 1200     c) 1500     d) 2000
+ *  Ans:    1200
+ *      -   Difference between simple and compound interest for a period of 
+ *          2 years= (p*r^2)/100, where P is the principal amount and r is the 
+ *          rate of interest.
+ *      -   p(r/100)^2=48 
+ *          r= 20% per annum.
+ *          p(20/100)^2=48 
+ *          P= Rs.1200. Hence, option (c) is correct.
+ */
+
+/**
+ *  Find SI?
+ *  -   If the compound interest on a certain amount at 20% per annum for 3 
+ *      years is Rs. 1092 then find the simple interest on the same amount 
+ *      at the same interest rate and for the same period.
+ *      a) Rs 1200  b) Rs 900   c) Rs 1100  d) None of these
+ *  Ans:    Rs 900
+ *      -   Let the principal amount is Rs. ‘p’, then according to the question:
+ *      -   p(1+r/100)^3-p=1092
+ *          (216/125)p-p=1092
+ *          91p/125=1092    
+ *          p=Rs. 1500
+ *          Simple interest= (1500×20×3)/100=Rs. 900
+ */

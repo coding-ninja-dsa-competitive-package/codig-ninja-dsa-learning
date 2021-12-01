016964c5b831cc27c6cf9fe499649ed922bcaf54
“tarunve” <tarun.verma151@gmail.com>
2021-12-01 15:36:37 +0530

    aptitude problems

---
+++ 04-aptitude/modules/008-Time-Ans-Work.cpp
@@ -0,0 +1,193 @@
+/**
+ *  Days Taken By A
+ *  -   A can do a piece of work in 12 days and B in 15 days. They work 
+ *      together for 5 days and then B left. The days taken by A to finish 
+ *      the remaining work is
+ *      a)  3   b) 5    c) 10   d) 12
+ *  Ans:    3
+ *      -   Let the total work be LCM of 12 & 15= 180.
+ *      -   Efficiency of A= 180/12= 15
+ *          Efficiency of B= 180/15=12
+ *          Total work done by A & B in first five days= (15+ 12)*5= 135.
+ *          Time taken by A to finish the remaining work= (180-135)/15=45/15=3 
+ *          days. Hence, option (1) is correct.
+ */
+
+/**
+ *  B Alone Days
+ *  -   A and B can do a piece of work in 20 days. A alone can do it in 24 days 
+ *      then in how many days can B alone do it?
+ *      a) 120  b) 60   c) 100  d) 30
+ *  Ans:    120
+ *      -   (A+B)’s 1 day work=(1/20),
+ *      -   A’s 1 day work=(1/24) 
+ *          so B’s 1 day work= (1/20)-(1/24)=(1/120). 
+ *          Hence no. of days=120.
+ *          Hence, option (a) is correct.
+ */
+
+/**
+ *  Total Time Together
+ *  -   A, B and C can do a work separately in 16, 32 and 48 days respectively. 
+ *      Total time taken by A, B and C together to finish the job?
+ *      a) 10.67 days   b) 9.33 days    c) 12.33 days   d) 8.72 days
+ *  Ans:    8.72 days
+ *      -   Let the total work be LCM of (16, 32, 48)= 96
+ *      -   Efficiency of A= 96/16= 6
+ *          Efficiency of B= 96/32= 3
+ *          Efficiency of C= 96/48= 2
+ *          Total efficiency= 6+ 3+ 2= 11
+ *      -   Total time taken by A, B and C together to complete the work=
+ *          96/11= 8.72 days.
+ */
+
+/**
+ *  Tank Empty Time
+ *  -   Two pipes can fill a tank with water in 15 and 12 hours respectively 
+ *      and a third pipe can empty it in 4 hours. If the pipes be opened in 
+ *      order at 8, 9 and 11 a.m. respectively, the tank will be emptied at
+ *      a) 11:400 am     b) 12:40am     c) 1:40pm   d) 2:40pm
+ *  Ans:    2:40 pm
+ *      -   Let the capacity of the tank = LCM of (15, 12, 4)= 60 liters.
+ *      -   1st pipe= 60/15= 4 liters per minute.
+ *          2nd pipe= 60/12= 5 liters per minute.
+ *          3rd pipe= 60/4= 15 liters per minute.
+ *          At 11 A.M.Tank filled by 3×4 +2×5=22 liters.	
+ *          At 11 A.M. all the three pipes are open 4+ 5- 15= -6 liters.
+ *      -   Tank will be emptied 22/6= 3 hours 40 minutes after 11 AM. 
+ *      -   Hence, tank will be emptied by 2: 40 PM. Hence, option (4) is correct.
+ */
+
+/**
+ *  People Efficiency
+ *  -   If 10 people can do a job in 20 days, then 20 people with twice the 
+ *      efficiency can do the same job in
+ *      a) 5 days   b) 10 days  c) 20 days  d) 40 days
+ *  Ans:    5 days
+ *      -   Number of people and efficiency both got twice hence required number 
+ *          of days= 20/(2×2)=5 days. Hence, option 1 is correct.
+ */
+
+/**
+ *  Days To Complete Work
+ *  -   If 72 men can build a wall of 280 m length in 21 days, how many men 
+ *      could take 18 days to build a similar type of wall of length 100 m?
+ *      a) 30   b) 10    c) 18  d) 28
+ *  Ans:    30
+ *      -   72×21 men-days= 280 m
+ *      -   Let 'x' men took 18 days to build the similar type of wall of 
+ *          length 100m.
+ *      -   72×21 men-days= 280 m
+ *          18×4×21men-days=280
+ *          18×30 men-days=100 m    
+ *          Hence the required answer must be 30 men. Hence, option (1) is correct.
+ */
+
+/**
+ *  Days To Complete Work
+ *  -   12 workers can do a piece of work in 300 days. At the end of 150 days, 
+ *      the number of workers employed is doubled. So in how many more days will 
+ *      the work be completed?
+ *      a) 50 days  b) 75 days c) 60 days   d) 80 days
+ *  Ans:    75 days
+ *      -   If x is the no. of more days taken to complete the work then 
+ *          according to the concept:
+ *              12×300-12×150=24x   
+ *          By solving we get x=75 days.
+ */
+
+/**
+ *  Men To Be Employed
+ *  -   Seventy-five men are employed to lay down a railway line in 3 months. 
+ *      Due to certain emergency conditions, the work was to be finished in 
+ *      18 days. How many more men should be employed to complete the work 
+ *      in the desired time?
+ *      a) 300  b) 325  c) 350  d) 375
+ *  Ans:    325
+ *      -   Total work= 75×3 men-months or 75 x 90 men days
+ *      -   Let ‘x’ more men employed to finish the work in 18 days.
+ *              75×90=(75+x)*18 
+ *      -   On solving, we get: x= 300. Hence, option (1) is correct.
+ */
+
+/**
+ *  Same Piece Of Work
+ *  -   If 4 men or 8 women can do a piece of work in 15 days, in how many days     
+ *      can 6 men and 12women do the same piece of work ?
+ *      a) 20 days  b) 5 days   c) 15 days  d) 30 days
+ *  ANs:    5 days
+ *      -   Let the efficiencies of 1 man and 1 woman be ‘m’ and ‘w’.
+ *              4m=8w 
+ *              m= 2w OR w= m/2.
+ *          Total work= 4m×15=60m.
+ *          Let 6 men and 12 women did the same job in ‘x’ days.
+ *              (6m+12w)*x=60m 
+ *              (6m+6m)*x=60m   
+ *              x=5 days.
+ *          Hence, option 2 is correct.
+ */
+
+/**
+ *  Finish Work
+ *  -   3 men and 4 boys can complete a piece of work in 12 days. 4 men 
+ *      and 3 boys can do the same work in 10 days. Then 2 men and 3 boys 
+ *      can finish the work in number of days is
+ *      a) 35/2 days    b) 60/11 days   c) 8 days   d) 22 days
+ *  Ans:    35/2 days
+ *      -    If the efficiencies of 1 man and 1 boy be m and b respectively 
+ *          then
+ *           According to the question: 12(3 m + 4 b)= 10(4 m+ 3 b)
+ *                  36 m + 48 b = 40 m + 30 b
+ *                  4 m = 18 b
+ *                  2 m = 9 b
+ *              ∴ 4 m + 3 b = 18b+ 3b= 21 b
+ *      	        Total work= 210b. 
+ *                  2men and 3 boys= 2m+3b=9b+3b=12b
+ *                  21b * 10 = 12b * d
+ *                  d= (21×10)/12=35/2 
+ */ 
+
+/**
+ *  Men Required
+ *  -   If 10 men complete a piece of work in 30 days, how many men are 
+ *      required to complete the same in 25 days?
+ *      a) 35   b) 18   c) 12   d) 20
+ *  Ans:    12
+ *      -   No. of  men required is inversely proportional to the no. of 
+ *          days required.  
+ *      -   Hence, M1/ M2= D2/D1 where M1=10, M2=? ,D1=30 , D2=25. 
+ *      -   Now, we get M2=12 by putting the values.
+ */
+
+/**
+ *  Shubham And Number Of Days
+ *  -   Shubham can do 25% of work in 10 days. How many days will he take to 
+ *      complete a work equal to 5 times this work?
+ *      a) 250 days     b) 200 days     c) 300 days     d) 350 days 
+ *  Ans:    200 days
+ *      -   Work done by Shubham in 10 days= W/4 if W is total work. 
+ *      -   Hence, he completes the work in 40 days so does 5 times of that 
+ *          work in 40 X 5=200 days. 
+ */
+
+/**
+ *  Days To Complete Work
+ *  -   10 men can dig a hole in 18 days. After 6 days, 5 more men joined. After 
+ *      how many days the remaining work would be completed?
+ *      a) 16 days  b) 13 days  c) 8 days   d) 12 days
+ *  Ans:    Solution: total work= 10 x 18= 180 men-days 
+ *      -   After 6 days remaining work= 180- 60= 120 men-days
+ *          Required time to complete the work= 120/15=8 days. 
+ */
+
+/**
+ *  Number Of Men To Finish Work
+ *  -   18 men working 8 hours a day can complete a work in 5 days. Working at 
+ *      a rate of 6 hours a day, the number of men required to finish the work 
+ *      in 10 days is
+ *      a) 12   b) 10   c) 15   d) 20
+ *  Ans:    12
+ *      -   Total work= 18×8×5=720 =6×10×x=60x, where x is the no. Of men 
+ *          required. 
+ *          On equating, we get x=12.
+ */

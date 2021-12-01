007f2f41c2db65275a724e9455fd44f3d072bb84
“tarunve” <tarun.verma151@gmail.com>
2021-12-01 15:36:33 +0530

    aptitude problems

---
+++ 04-aptitude/modules/007-Ratio-Proportaion-Variation-2.cpp
@@ -0,0 +1,175 @@
+/**
+ *  Coins Of Denominations
+ *  -   A boy has a few coins of denominations 50 paise, 25 paise and 10 paise 
+ *      in the such a way that 6 times the number of 50 paise coins is equal to 
+ *      three times of 25 paise coins and two times of 10 paise coins. If the 
+ *      total amount of the coins is Rs. 6.50, the number of 10 paise coins is
+ *      a) 5    b) 10   c) 15   d) 20
+ *  Ans:    15
+ *      -   According to the question: 6 times the number of 50 paise coins is 
+ *          equal to three times of 25 paise coins and two times of 10 paise 
+ *          coins.
+ *      -   50 paise coins : 25 paise coins: 10 paise coins= 1/6 : 1/3 : 
+ *          ½= 1: 2: 3
+ *      -   Let the numbers of 50 paise, 25 paise and 10 paise are x, 2x and 3x.
+ *              50x+25×2x+10×3x=650 
+ *              130x=650 
+ *              x=5 
+ *              2x=10,
+ *              3x=15 
+ *          Number of 10 paise= 15. Hence, option (c) is correct. 
+ */
+
+/**
+ *  Sum Of Runs
+ *  -   A team of four players scored 100 runs together. A scored half as many 
+ *      runs as B, C’s score was 1.5 times of B and D’s score was twice of B. 
+ *      Find the sum of runs scored by A and C.
+ *      a) 10   b) 30   c) 40   d) None of these
+ *  Ans:    40
+ *      -   let the runs scored by A, B, C and D were a, b, c and d respectively.
+ *              a= b/2, c= 1.5b, d= 2b      
+ *              a: b: c: d= ½ : 1: 1.5: 2           
+ *              a: b: c: d= 1: 2: 3; 4
+ *      -   Hence, the runs scored by A and C= 100×((1+3)/(1+2+3+4))=40.
+ */
+
+/**
+ *  Vessels
+ *  -   Two vessels A and B contain spirit and water mixed in the ratio 5:2 and 
+ *      7:8 respectively. If the equal quantity of both mixtures are mixed in 
+ *      a vessel C then what would be the ratio of spirit and water in vessel C?
+ *      a) 44:62    b) 64:43    c) 62:43    d) None of these
+ *  Ans:    62:43
+ *      -   Solution: let the initial quantity of mixture in both vessels be 
+ *          105 liters.
+ *      -   In vessel A: Spirit= 55+2×105=75 liters. Water= 105- 75= 30 liters.
+ *      -   In vessel B: Spirit= 77+8×105=49 liters. Water= 105- 49= 56 liters.
+ *      -   Required ratio= 75+49:30+56=124:86=62:43
+ *          Hence, option (c) is correct. 
+ */
+
+/**
+ *  Milk And Water
+ *  -   The milk and water in two vessels A and B are in the ratio 4:3 and 2:3 
+ *      respectively. If equal quantities of mixture from both vessels are 
+ *      taken in a different vessel C, then what is the ratio of milk and 
+ *      water in the vessel C?
+ *      a) 5:7  b) 7:5  c) 1/5:1/8  d) 17:18
+ *  Ans:    17:18
+ *      -   Let the initial quantity of mixture in both vessels be 35 liters.
+ *      -   In vessel A: Spirit= 44+3×35=20 liters. Water= 35- 20= 15 liters.
+ *          In vessel B: Spirit= 22+3×35=14 liters. Water= 35- 14= 21 liters.
+ *          Required ratio= 20+14:15+21=34:36=17:18
+ *          Hence, option (d) is correct. 
+ */
+
+/**
+ *  Ratio Of Incomes
+ *  -   The ratio of incomes of Piku and Qasim is 3:4 and the ratio of their 
+ *      expenditures is 2:3. If both of them save Rs6000, the income of Piku is
+ *      a) Rs 20000     b) Rs 12000     c) Rs 18000     d) Rs 24000
+ *  Ans:    Rs 18000
+ *      -   Let the incomes of Piku and Qasim be Rs3x and 4x respectively.
+ *      -   Again, let their expenditures be Rs2y and 3y respectively.
+ *      -   According to the question,
+ *              3x – 2y = 6000	…(i)and
+ *              4x – 3y = 6000	…(ii)
+ *          From equations (i) and (ii)
+ *              3x – 2y = 4x – 3y
+ *              or, 4x – 3x = 3y – 2y
+ *              or, x = y
+ *          From equations (i)
+ *              3x – 2x = 6000
+ *              x = 6000
+ *          The income of Piku = Rs3x = Rs(36000) = Rs18000
+ */
+
+/**
+ *  Incomes of Abhay And Bishan
+ *  -   The incomes of Abhay and Bishan are in the ratio 2:3 and their 
+ *      expenditures are in the ratio 1:2. If each saves Rs24,000, find         
+ *      A’s income?
+ *      a) Rs 24000     b) Rs 72000     c) Rs 19200    d) Rs 48000
+ *  Ans:    Rs 48000
+ *      -   Let the incomes of Abhay and Bishan be Rs2x and Rs3x and their 
+ *          expenditures be Rs y and Rs 2y respectively.  
+ *              ∴ 2x – y = 24000	…(i)
+ *              And 3x – 2y = 24000	…(ii)   
+ *          By equations (i)*2 – (ii)
+ *              4x – 2y – 3x + 2y = 24000
+ *              x = 24000
+ *              ∴ Abhay’s income = 2*24000 = Rs48000
+ */
+
+/**
+ *  Ratio Of Initial And Final Values
+ *  -   If x is directly proportional to y and x is increased by 30% then what 
+ *      would be the ratio of initial and final values of y?
+ *      a) 10:11    b) 10:13    c) 5:7      d) None of these
+ *  Ans:    10:13
+ *      -   x~y
+ *          After increasing x by 30% the value of y would be increased
+ *          proportionally. Required ratio= y: 1.3y= 1: 1.3=10: 13
+ */ 
+
+/**
+ *  Ratio Of Values Of y
+ *  -   If x is inversely proportional to y and x is increased by 10% then 
+ *      what would be the ratio of initial and final values of y?
+ *      a) 10:11    b) 10:13    c) 5;7  d) 11:10
+ *  Ans:     11:10
+ *      -   (x~1/y) or (y~1/x) 
+ *          y1:y2 =(1:(1/1.1))=1:(10/11)=11:10 
+ *          Hence, option (d) is correct.
+ */
+
+/**
+ *  Distance Covered
+ *  -   A train travels 10 km at a particular speed in a particular time. If 
+ *      speed is increased by 100% and time is reduced by 50% then find the
+ *      distance covered by the train.
+ *      a) 10 km    b) 5 km     c) 15 km    d) None of these
+ *  Ans:    10 km 
+ *      -   Distance in directly proportional to speed and time.
+ *          New speed= old speed+ 100% of old speed= Twice of the old speed
+ *          Similarly new time= ½ of the old time.
+ *          Distance= 2*Speed x ½ time= Speed x time= 10 km. Hence, 
+ *          option (a) is correct.
+ */
+
+/**
+ *  Find x?
+ *  -   If x is directly proportional to y and x= 10 when y= 3, then 
+ *      find x when y= 9.
+ *      a) 3.33     b) 3    c) 30   d) None of these
+ *  Ans:    30 
+ *      -   x~y 
+ *          x1:x2=y1:y2 
+ *          10: x2= 3: 9
+ *          x2= 30. Hence, option (c) is correct.
+ */
+
+/**
+ *  Find x when y is given?
+ *  -   If x is inversely proportional to y and x= 100 when y= 10, then 
+ *      find x when y= 20.
+ *      a) 50   b) 3    c) 30   d) None of these
+ *  Ans:    50  
+ *      -   x~1/y   
+ *          x1:x2=y2:y1
+ *          100: x2= 20: 10
+ *          x2= 50. Hence, option (a) is correct.
+ */
+
+/**
+ *  Find value of B?
+ *  -   A is directly proportional to the square of B. for A= 10, B= 50. 
+ *      Then, find the value of B, when A= 5.
+ *      a) 50    b) 25  c) 50/sqrt(2)   d) None of these
+ *  Ans:    50/sqrt(2)
+ *      -   A~B^2 
+ *          A1:A2=B1^2:B2^2 
+ *          10: 5= 50^2:B2^2
+ *          B2= 50/sqrt(2) 
+ */

9a70cf4283383fa18b6d3cf38e1df46fcfdda3a2
“tarunve” <tarun.verma151@gmail.com>
2021-12-01 15:36:40 +0530

    aptitude problems

---
+++ 04-aptitude/modules/009-Reasoning-1.cpp
@@ -0,0 +1,225 @@
+/**
+ *  Find next sequence?
+ *  -   JAF, JEF, JIF, JOF, ?
+ *      a) JAG  b) JET  c) JUF  d) JIT
+ *  Ans:    JUF 
+ *      -   #### The middle letters which are vowels have an increasing trend 
+ *          of A, E, I, O, U and remaining letters have been retained. Hence 
+ *          the answer would be JUF. Option (c) is correct.
+ */
+
+/**
+ *  Find next alphabets?
+ *  -   BAZ, DBY, FCX, ?
+ *      a) FXW  b) EFX  c) FEY  d) HDW
+ *  Ans:    HDW 
+ *      -   The 1st letter of the world increasing by +2, 2nd letter increasing 
+ *          by +1 and the 3rd letter is decreasing by -1.
+ *      -   Hence, the missing letter HDW. Option (d) is correct.
+ */
+
+/**
+ *  Find next Number?
+ *  -   5, 12, 23, 50, 141, ?
+ *      a) 488  b) 473  c) 425  d) 392
+ *  Ans:    488
+ *      -   4=41, 16=42, 64=43 
+ *          Hence, the next term =141+91+44= 141 +91 +256 =488.
+ *          Hence, option (a) is correct.
+ */
+
+/**
+ *  Next Number?
+ *  -   13, 16, 22, 31, 43, ?
+ *      a) 60   b) 58   c) 55   d) 56   
+ *  Ans:    58
+ *      -   Pattern of the series is +3, +6, +9, +12, …
+ *          Hence, the missing term= 43+ 15= 58. Hence, option (b) is correct.
+ */
+
+/**
+ *  Next Number Again?
+ *  -   50, 90, 150, 240, 375, ?
+ *      a) 570    b) 577.5  c) 580  d) None of these
+ *  Ans:    577.5
+ *      -   90 = 50 +40,        
+ *          150 = 90 +60,          
+ *          240 = 150 +90
+ *          375 = 240 +135.
+ *      -   We can see 60 = 40 ×1.5,          
+ *              90 = 60 ×1.5,                       135 = 90 ×1.5,  
+ *      -   Hence, the required term = 375 + 135 × 1.5  = 577.5
+ *          Hence, option (b) is correct.
+ */
+
+/**
+ *  Find number?
+ *  -   4, 6, 16, 54, 224,?
+ *      a) 1125     b) 1130     c) 1135     d) 1140
+ *  Ans:    1130    
+ *      -   6 = 4 ×1 +2,      
+ *          16 = 6 ×2 +4,
+ *          54 = 16 ×3 +6,         
+ *          224 = 54 ×4 +8,
+ *          ? = 224 × 5 +10 = 1130.
+ *          Hence, option (b) is correct.
+ */
+
+/**
+ *  Find missing number?
+ *  -   2, 10, 30, 68, 130, ..................?
+ *      a) 225  b) 224  c) 223  d) 222
+ *  Ans:    222
+ *      -   (1)^3 +1 =2,    
+ *          (2)^3 +2 =10,      
+ *          (3)^3 +3 =30,       
+ *          (4)^3 +4 =68
+ *          (5)^3 +5 =130,    
+ *          ? = (6)^3 +6  =222.
+ *          Hence, option (d) is correct.
+ */
+
+/**
+ *  Syllogism-1
+ *  -   Statements: All A are B. All B are C. All C are D
+ *      Conclusion:
+ *          I. All B are D.
+ *          II. All C are A.
+ *          III. All A are D.
+ *          IV. All D are B
+ *      a) Only I and II follow     b) Only I and III follow    c) Only II and  
+ *          III follow  d) All follow
+ *  Ans:    Only I and III follow.
+ */
+
+/**
+ *  Predict The Correct Thing?
+ *  -   Statements: Some eyes are ears. Some ears are lungs. All lungs are hands
+ *      Conclusion:
+ *          I. Some hands are eyes.
+ *          II. Some hands are ears
+ *          III. Some lungs are eyes
+ *          IV. No hand is eye
+ *      a) None follow  b) Only IV follow   c) Only II follows  d) Only III follows
+ *  Ans:    Only II follows
+ */     
+
+/**
+ *  Select The Correct Statement?
+ *  -   Statements: All liquids are solids. Some solids are gases. All gases 
+ *                  are clouds.
+ *      Conclusion:
+ *          I. Some clouds are solids.
+ *          II. Some clouds are liquids
+ *          III. Some gases are liquids.
+ *          IV. Some solids are clouds
+ *      a) None follows     b) Only I and II follow     c) Only III and IV follow
+ *      d) Only I and IV follow 
+ *  Ans:    Only I and IV follow 
+ */
+
+/**
+ *  Predict About Statements?
+ *  -   Statements: All A are B. No B is C. Some D are C.
+ *      Conclusion:
+ *          I. Some D are A.
+ *          II. Some D are B
+ *          III. Some A are C.
+ *      a) Only I follows.  b) Only III follows. c) None of these. 
+ *      d) Only II and III follow.
+ *  Ans:    None of these
+ */
+
+/**
+ *  Get The Conclusion?
+ *  -   Statements: Some snaps are whatsapp. All Hikes are whatsapp. All 
+ *      whatsapp are facebook.
+ *      Conclusion:
+ *          I. Some facebook are snaps.
+ *          II. All hikes are facebook
+ *          III. Some snaps are hikes.
+ *          IV. Some snaps are facebook
+ *      a) All follow   b) Only I, II and III follow c) Only I, II and IV follow. 
+ *      d) Only III and IV follow
+ *  Ans:    Only I, II and IV follow.
+ */
+
+/**
+ *  Statements And Conclusion?
+ *  -   Statements: Some cows are deer. No deer is dog. Some dogs are fox.
+ *      Conclusions:
+ *          I. Some cows are fox.
+ *          II. Some foxes are deer.
+ *      a) None follows b) Only I follows c) Either I or II follows d) Only II follows
+ *  Ans:    None follows
+ */
+
+/**
+ *  Select The Correct Option?
+ *  -   Some points are not scales. No scale is circle. Some circles are triangles.
+ *      Conclusions:
+ *          I. No point is circle.
+ *          II. No triangle is point.
+ *      a)  None follows b) Only I follows c) Either I or II follows d) Only II follows
+ *  Ans:    None follows
+ */
+
+/**
+ *  Leaves, Branches And Roots?
+ *  -   Some leaves are fruits. All branches are fruits. Some roots are 
+ *      branches Conclusions:
+ *          I. Some roots are fruits.
+ *          II. Some branches are leaves.
+ *          III. No leaf is branch. 
+ *          IV. Some leaves are roots.
+ *      a) Either II or III and I follow    b) Only I follows
+ *      c) Only either II or III follows    d) Only I and III follow
+ *  Ans:    Either II or III and I follow
+ */ 
+
+/**
+ *  Decode it?
+ *  -   In a certain code ‘SUDHIR’ is written as ‘HIRSUD’
+ *      How would ‘VISHES’ be written in that code?
+ *      a) VISHSE   b) HESVIS   c) VISEHS   d) HESIVS
+ *  Ans:    HESVIS
+ */
+
+/**
+ *  Decode again?
+ *  -   In a certain code ‘PURPOSE’ is written as ‘UPPRSOE’. How would 
+ *      ‘SUDHI’ is written in that code.
+ *      a) USHDI    b) SUDIH    c) USDIH    d) None Of These
+ *  Ans:    USHDI
+ */
+
+/**
+ *  Coding Decoding
+ *  -   In a certain code ‘SUDHIR’ is written as ‘QTBGGQ’. How would ‘ARUN’ 
+ *      be written in that code?
+ *      a) QYSM b) PQRS c) YQSM d) None Of These
+ *  Ans:    YQSM
+ */
+
+/**
+ *  Find pattern?
+ *  -   If AMAN is written as ‘BNBO’ in a certain code then ARUN is written as
+ *      a) BPTM b) BSVM c) BVSM d) None Of These 
+ *  Ans:    None of these
+ */
+
+/**
+ *  Find code?
+ *  -   If ‘AMAN’ is written as ‘CLCM’ in a certain code than in the same code 
+ *      ‘ARUN’ is written as:
+ *      a) CQWO b) CQVM c) CPWM d) CQWM 
+ *  Ans:    CQWM
+ */
+
+/**
+ *  Decode the number?
+ *  -   In a certain code if ‘1000’ is written as ‘1728’ and 125 is written 
+ *      as ‘343’. In the same code ‘512’ is written as:
+ *      a)  729 b) 343  c) 216  d) 1000 
+ *  Ans:    1000
+ */

6ad2c336419da1f0c42d9b07f1ad796ca0d21e6c
“tarunve” <tarun.verma151@gmail.com>
2021-12-08 15:07:26 +0530

    aptitude problems

---
+++ 04-aptitude/modules/014-Applications-Of-Time-Speed-Distance.cpp
@@ -0,0 +1,242 @@
+/**
+ *  Speed Of Train
+ *  -   A train, 300m long, passed a man, walking along the line in the same 
+ *      direction at the rate of 3 km/hr in 33 seconds. The speed of the 
+ *      train is:
+ *      a) 30 km/hr     b) 32 km/hr     c) (360/11) km/hr d) (393/11) km/hr 
+ *  Ans:    393/11 km/hr
+ *  -   If the speed of the train be s km/hr, then relative speed = (s – 3)km/hr 
+ *      or  (s-3)*(5/18)m/sec
+ *          ∴300/(s-3)*(5/18)=33
+ *          ⇒5400=33×5(s-3)
+ *          ⇒11s-33=360⇒s=393/11 km/hr
+ */
+
+/**
+ *  Find train speed?
+ *  -   A train 100 metres long meets a man going in opposite direction at 
+ *      5 km/hr and passes him in (36/5)seconds. What is the speed of the train 
+ *      in km/hr?
+ *      a) 45 km/hr  b) 60 km/hr c) 55 km/hr d) 50 km/hr
+ *  Ans:    45 km/hr
+ *      -   Speed of train = x km/hr
+ *          Relative speed = (x + 5) km/hr
+ *          Length of train = 100m=0.1 km.
+ *          ∴0.1/(x+5)=36/(5×60×60)
+ *          1/(x+5)=1/50
+ *          ⇒x+5=50
+ *          ⇒x=45 km/hr 
+ */
+
+/**
+ *  Speed passing telegraph post    
+ *  -   A train, 120 m long, takes 6 seconds to pass a telegraph post, the 
+ *      speed of train is
+ *      a) 72 km/hr  b) 62 km/hr c) 55 km/hr d) 85 km/hr
+ *  Ans:    72 km/hr 
+ *      -   Speed of train = Length of train/Time taken in crossing the pole  
+ *          =120/6=20 m/sec = 20×18/5=72 km/hr. Hence, option a is correct.
+ */ 
+
+/**
+ *  Average speed of whole journey
+ *  -   A train goes from Ballugunge to Sealdah at an average speed of 
+ *      20 km/hr and comes back at an average speed of 30 km/hr. The average 
+ *      speed of the train for the whole journey is
+ *      a) 27 km/hr   b) 26 km/hr c) 25 km/hr d) 24 km/hr 
+ *  Ans:    24 km/hr
+ *  -   Required average speed = 2S1S2/(S1+S2)=2×30×20/(30+20)=24 km/hr
+ *      Hence, option d is correct.
+ */
+
+/**
+ *  Time To Cross Each Other
+ *  -   Two trains 108m and 112m in length are running towards each other 
+ *      on the parallel lines at a speed of 45 km/hr and 54 km/hr respectively.
+ *      To cross each other after they meet, it will take
+ *      a) 12 sec b) 9 sec c) 8 sec d) 10 sec
+ *  Ans:    8 sec
+ *      -   Relative speed = 45 + 54 = 99 km/hr= 99×5/18 m/sec or 55/2 m/sec
+ *          ∴ Required time = ((108+112)/(55/2))=(220×2)/55=8 seconds. Hence, 
+ *          option(c) is correct.
+ */
+
+/**
+ *  Speed Of Faster Train
+ *  -   Two trains, each 100 m long, moving in opposite directions, cross each 
+ *      other in 8 seconds. If one is moving twice as fast the other, then the 
+ *      speed of the faster train is:
+ *      a) 50 km/hr b) 55 km/hr c) 60 km/hr  d) 65 km/hr
+ *  Ans:    60 km/hr
+ *      -   Let the speed of the slower train be x m/sec .
+ *          Then, speed of the faster train be 2x m/sec .
+ *          Relative speed of both Trains = (x + 2x) m/sec = 3x m/sec .
+ *          Now according to the question:
+ *              (100+100)/8=3x⇔24x=200⇔x=25/3 m/s
+ *          So, speed of the faster train = 2×25/3 m/sec or(50/3*18/5) km/hr 
+ *          = 60 km/hr. Hence, option c is correct.
+ */
+
+/**
+ *  Original Speed Of Train
+ *  -   A train travels at a certain average speed for distance of 63 km and 
+ *      then travels a distance of 72 km at an average speed of 6 km/hr more 
+ *      than its original speed. If it takes 3 hours to complete the total 
+ *      journey, what is the original speed of the train in km/hr.?
+ *      a) 24   b) 33 c) 42  d) 66
+ *  Ans:    42
+ *      -   The following equation can be formed from the equation,
+ *              63/s + 72/(s+6) =3
+ *          Solve the equation to get,
+ *              s = 42 km/hr. Hence, option c is correct.
+ */
+
+/**
+ *  Distance Between Two Points
+ *  -   A boat moves with a speed of 5 kmph in still water. When the river 
+ *      flows at 1kmph, the boat takes 80 minutes to go from a point A to B 
+ *      and come back. What is the distance between the two points?
+ *      a) 3 km    b) 2.4 km    c) 3.2 km   d) 2.8 km
+ *  Ans:    3.2 km
+ *      -   As the distance is constant here, so the speed is inversely 
+ *          proportional to the time taken. 
+ *          Downstream speed = (5+1) kmph = 6 kmph
+ *          Upstream speed = (5 – 1) kmph = 4 kmph
+ *          Ratio of Downstream speed: Upstream speed = 6:4 
+ *          So, the time taken in Downstream journey: Upstream journey = 4:6
+ *          So, the time taken in Downstream journey = 4/10 × 80 = 32 minutes
+ *          So, the distance = 32/60 × 6 = 3.2 kms. Hence, option c is correct.
+ */ 
+
+/**
+ *  How Far Is The Place?
+ *  -   Arjun can swim at 10kmph in still water. If the velocity of water is 
+ *      4kmph and it takes him 10 hours to swim to a place and come back, how 
+ *      far is the place?
+ *      a) 24 km    b) 32 km c) 42 km d) 48 km
+ *  Ans:    42 km
+ *      -   Let the place is D km away from the original position of Arjun. 
+ *          According to the question: 
+ *              ((D/(10+4))+(D/(10-4))=10  OR D/14+D/6=10
+ *          Now by putting the options in the above equation or by solving the
+ *          above equation we get D= 42 km. Hence, option c is correct.
+ */
+
+/**
+ *  Speed Of Current
+ *  -   A boat goes 6 km in one hour in still water, but takes thrice as much 
+ *      time in going the same distance against the current. The speed of 
+ *      current (in kmph) is:
+ *      a) 2    b) 3    c) 4    d) 5
+ *  Ans:    4
+ *      -   Speed of boat in still water =  6 km / hour
+ *          Let the distance = x km
+ *          Time taken = x/6 hrs
+ *          Time is going the same distance against the current  = 3x/6=x/2 hrs
+ *          Let the speed of the current  = y km/hr
+ *          Speed against the current = 6−y km/hr
+ *          Time taken for distance x= x/( 6−y) hrs.
+ *          hence   x/2=x/( 6−y)
+ *          so, ( 6−y) = 2
+ *          and  y= 4 km/hr 
+ *          Hence Speed pf the current  = y km/hr =4 km/hr
+ */
+
+/**
+ *  Coincide Each Other
+ *  -   At what time between 2 and 3 o'clock will the hands of a clock 
+ *      coincide each other?
+ *      a) 10 minutes 120/11 seconds    b) 10 minutes 600/11 seconds 
+ *      c) 9 minutes 20/11 seconds      d) None of These
+ *  Ans:    10 minutes 600/11 seconds
+ *      -   if 3600 is equivalent to 60 km.
+ *          Speed of the minute hand= 60 kmph
+ *          Speed of the hour hand= 5 kmph.
+ *          Initial distance between minute and hour hand= 10 km.
+ *          Final distance between minute and hour hand= 0 km.
+ *          Relative speed of minute and hour hand= 55 kmph
+ *          Required time= 10/55hours or 600/55 minutes or 10 minutes 
+ *          600/11 seconds.
+ *          Hence both hands coincide each other after 10 minutes 600/11 
+ *          seconds. Hence, option b is correct.
+ */
+
+/**
+ *  Form An Angle
+ *  -   At what time between 2 and 3 o'clock will the hands of a clock form 
+ *      an angle of 900 from each other?
+ *      a) 2:27:81.81    b) 2:27:16.36  c) 2:27 d) None Of These
+ *  Ans:    2:27:16.36
+ *      -    Initial distance between minute and hour hand= 10 km.
+ *          Final distance between minute and hour hand= 15 km.
+ *          Relative speed of minute and hour hand= 55 kmph
+ *          Required time= (15+10)/55=25/55hours or (25*60)/55 minutes or 
+ *          27 minutes 16.36 seconds. Hence, option b is correct.
+ */
+
+/**
+ *  Number Of Right Angle
+ *  -   Find the number of right angle formed by a clock in a day?
+ *      a) 48   b) 44 c) 23 d) 24
+ *  Ans:    44
+ *      -   A clock makes 2 right angles between any 2 hours. thus, for 
+ *          instance there are 2 right angles formed between 12 and 1 or 
+ *          between 1 and 2 and so on. Therefore clock makes 4 right angles 
+ *          in two hours time period. But this statement is not true for 2-4 
+ *          and for 8-10, because with in this interval there are not 4 but 3 
+ *          right angles. This happen because second right angle between 2 
+ *          and 3 and 1st right angle between 3-4 are same. Similarly second 
+ *          right angle between 8-9 and first right angle between 9-10 are same.
+ *      -   Hence there are total 48- 4= 44 right angles formed by the clock 
+ *          in a 24 hours period. Hence, option b is correct.
+ */
+
+/**
+ *  Ratio Of Speeds
+ *  -   In a 100 m race Aman wins over Bharat by 8 m. or by 1 s. Bharat can 
+ *      give a start of 10m to Chiraag in 100 m race. Now answer the following 
+ *      questions: Find the ratio of speeds of Aman, Bharat, Chirag.
+ *      a) 1000:900:828 b) 1000:920:828  c) 1000:920:818 d) None Of These
+ *  Ans:    1000:920:828
+ *      -   Ratio of speeds of Aman and Bharat = 100:100-8=100:92
+ *          Ratio of speeds of Bharat and Chirag = 100:100-10=100:90
+ *          So ratio of speeds Anuj, Bharat, Chirag =1000:920:828
+ *          Hence, option b is correct.
+ */
+
+/**
+ *  By how much time?
+ *  -   In a 100 m race Aman wins over Bharat by 8 m. or by 1 s. Bharat can 
+ *      give a start of 10m to Chiraag in 100 m race. Now answer the following 
+ *      questions:
+ *      By how much time Aman will win over Chirag in a 1 km race.
+ *      a) 20.5s b) 21.5s  c) 22s d) None Of These
+ *  Ans;    21.5s
+ *      -   Ratio of speeds Anuj, Chirag =1000:828
+ *          So in a 1 km race Anuj will win by 172 m.  speed of Aman = 81=8m/s
+ *          So Aman will win over Chirag by 1728=21.5 s. Hence, option b is correct.
+ */
+
+/**
+ *  Length Of Race Track
+ *  -   If the ratio of speeds of A and B is 3: 5 and A loses the race by 50 m, 
+ *      then what is the length of the race track?
+ *      a) 100m b) 125m c) 150m d) 175m
+ *  Ans:    125m
+ *      -   In a 5 m race B will win the race by 2 m. (Ratio of speeds of A 
+ *          and B is 3: 5)
+ *          So if B will win the race by 50 m then the length of race track 
+ *          = 50×5/2=125 m
+ *          Hence, option b is correct.
+ */
+
+/**
+ *  Length Of Track
+ *  -   The ratio of time taken by A and B to run a certain distance is 1: 3 
+ *      and A wins the race by 100 m then the length of race track is:
+ *      a) 100m b) 125m c) 150m d) 175m
+ *  Ans:    150m
+ *      -   Ratio of speed of A and B is 3:1.
+ *          So in a 3m race A wins by 2 m. so when A wins by 100m then length 
+ *          of the race track = 	100×3/2=150 m. Hence, option c is correct.
+ */

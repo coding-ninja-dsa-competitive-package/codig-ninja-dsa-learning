094ef655ef31fc0c5ebc2a981885f36590266b08
“tarunve” <tarun.verma151@gmail.com>
2021-12-08 15:07:19 +0530

    aptitude problems

---
+++ 04-aptitude/modules/012-Time-Speed-Distance.cpp
@@ -0,0 +1,238 @@
+/**
+ *  Conversion  
+ *  -   5km/hr=?    
+ *      a) 25/6 m/s     b) 50/3 m/s     c) 25/18 m/s    d) 25/4 m/s
+ *  Ans:    25/18 m/s
+ *      -   5 km/hr= 5×5/18=25/18 m/s
+ */
+
+/**
+ *  Calculate Speed 
+ *  -   An athlete runs 200 metres race in 24 seconds. His speed (in km/ hr) is :
+ *      a) 20   b) 24   c) 28.5     d) 30
+ *  Ans:    30 
+ *      -   200 meters= 0.2 km
+ *          24 seconds= 24/(60×60) hours
+ *          Required speed= 0.2/(24/(60×60))=30 km/hr. Hence, option (d) is correct.
+ */
+
+/**
+ *  Calculate Time
+ *  -   If a man covers 100 km in 10 hours, then how much time would he take to 
+ *      cover 150 km distance at the same speed?
+ *      a) 15 hours     b) 20 hours     c) 12 hours     d) None
+ *  Ans:    15 hours
+ *      -   As the speed is constant, hence time is directly proportional to 
+ *          distance. 100/150=10/t on solving we get t= 15 hours.
+ */
+
+/**
+ *  Time To Cover Distance
+ *  -   A man covers a particular distance in 10 hours at a speed of 20 kmph. 
+ *      If he increases his speed by 50% then in what time would he take to 
+ *      cover the same distance?
+ *      a) 6.67 hours   b) 7 hours  c) 8 hours  d) None
+ *  Ans:    6.67 hours
+ *      -   As the distance is constant and
+ *              speed ~ 1/time
+ *              Speed 1/ Speed 2= Time 2/ Time 1
+ *              20/30= time 2/10
+ *              Time 2= 20/3= 6.67 hours. Hence, option (a) is correct.
+ */
+
+/**
+ *  How early will reach?
+ *  -   If a man walks 20 km at 5 km/ hr, he will be late by 40 minutes. If 
+ *      he walks at 8 km/hr, how early from the fixed time will he reach?
+ *      a) 15 minutes   b) 25 minutes   c) 50 inutes    d) 1.5 hours
+ *  Ans:    50 minutes
+ *      -   Let the correct time to reach at ‘t’ minutes.
+ *          t= 20/5-40/60= 4-2/3=10/3 hours or 200 minutes.
+ *          Total time required to cover 20km at 8 kmph= 20/8= 2.5 hours = 150 min
+ *          Required time= 200- 150= 50 minutes. Hence, option (c) is correct.
+ */
+
+/**
+ *  Distance To School
+ *  -   A boy is late by 9 minutes if he walks to school at a speed of 4 
+ *      km/hour. If he walks at the rate of 5 km/hour, he arrives 9 minutes 
+ *      early. The distance to his school is
+ *      a) 9 km     b) 5 km     c) 4 km     d) 6 km
+ *  Ans:    6 km 
+ *      -   Let the time taken by boy to reach school on time be ‘t’ hours.
+ *          According to the question:
+ *              4(t+ 9/60)= 5(t-9/60)
+ *              t= 36/60+45/60=81/60 hours.
+ *          Required distance= 4(t+ 9/60)=4(81/60+9/60)=4×90/60=6 km. Hence, 
+ *          option (d) is correct. 
+ */
+
+/**
+ *  Distance Between A and B
+ *  -   A train covers a distance between station A and station B in 45 minutes. 
+ *      If the speed of the train is reduced by 5 km/hr, then the same distance 
+ *      is covered in 48 minutes. The distance between station A and B is
+ *      a) 60 km    b) 64 km    c) 80 km    d) 55 km
+ *  Ans:    60 km 
+ *      -   Let the distance between Station A and B be ‘d’ km and the original 
+ *          velocity of the train be ‘v’ kmph.
+ *      -   According to the question:
+ *              d/v=45/60 or v=(4/3)*d and d/(v-5)=48/60 or v-5=5d/4
+ *          On solving the above two equations we get: d=60 km. Hence, 
+ *          option (a) is correct.      
+ */
+
+/**
+ *  Time of Apart
+ *  -   Two persons ride towards each other from two places 55 km apart, one 
+ *      riding at 12km/hr and the other at 10 km/hr. In what time will they be 
+ *      11 km apart?
+ *      a) 2 hours and 30 minutes   b) 1 hour and 30 minutes 
+ *      c) 2 hours  d) 2 hours nad 45 minutes
+ *  Ans:    2 hours
+ *      -   Relative speed of two persons= 12+ 10= 22 kmph.
+ *          Relative distance covered by them= 55- 11= 44 km.
+ *          Required time= 44/22= 2 hours. Hence, option (c) is correct.
+ */
+
+/**
+ *  Time When They Meet
+ *  -   The distance between two cities A and B is 330 km. A train starts 
+ *      from A at 8 a.m. and travelstowards B at 60 km/hr. Another train 
+ *      starts from B at 9 a.m. and travels towards A at 75 km/hr. At what 
+ *      time do they meet?
+ *      a) 10 a.m.  b) 10:30 a.m.   c) 11 a.m.  d) 11:30 a.m.
+ *  Ans:    11 a.m.
+ *      -   At 9 A.M. distance between two trains= 330- 60=270 km
+ *          Relative speed of both trains= 60+ 75=135 km/hr.
+ *          Now, both trains would meet after 270/135= 2 hours. Hence, 
+ *          option (c) is correct.
+ */
+
+/**
+ *  Speed Of Truck
+ *  -   A bus moving at a speed of 45 km/hr overtakes a truck 150 metres 
+ *      ahead going in the same direction in 30 seconds. The speed of the 
+ *      truck is
+ *      a) 27 km/h  b) 24 km/h   c) 25 km/h     d) 28 km/h
+ *  Ans:    27 km/h
+ *      -   Relative speed of Bus and Truck= 150/30= 5 m/s or 18 kmph.
+ *          Speed of bus- speed of truck= 18
+ *          45- Speed of truck= 18
+ *          Speed of truck= 45- 18= 27 kmph. Hence, option (a) is correct.
+ */
+
+/**
+ *  Time Required
+ *  -   The distance between two cities A and B is 100km. and the speeds of 
+ *      Ram and Anuj are 15 km/h and 25km/h respectively. Initially both are 
+ *      at A and start moving toward B. If they move between A and B to and 
+ *      fro then answer the question:
+ *  -   Find the time required for the 1st meeting.
+ *      a) 3h   b) 4h   c) 5h   d) None 
+ *  Ans:    5h
+ *      -   The total distance covered by Anuj and Ram at the 1st meeting= 200km.
+ *          Combined speed of Anuj and Ram = 15km/h+ 25km/h= 40km/h
+ *          Time required for the 1st meeting = 200/ 40 = 5 h. Hence, 
+ *          option (c) is correct.
+ */
+
+/**
+ *  Distance Covered
+ *  -   The distance between two cities A and B is 100km. and the speeds of 
+ *      Ram and Anuj are 15 km/h and 25km/h respectively. Initially both are 
+ *      at A and start moving toward B. If they move between A and B to and 
+ *      fro then answer the question:
+ *  -   Distance covered by Anuj in 1st meeting.
+ *      a) 125 km   b) 150 km   c) 250 km   d) None 
+ *  Ans:    125 km
+ *      -   #### Distance covered by Anuj in 1st meeting = 25×5=125km. Hence, 
+ *          option (a) is correct.
+ */
+
+/**
+ *  Distance Covered
+ *  -   The distance between two cities A and B is 100km. and the speeds of 
+ *      Ram and Anuj are 15 km/h and 25km/h respectively. Initially both are 
+ *      at A and start moving toward B. If they move between A and B to and 
+ *      fro then answer the question:
+ *  -   Ratio of distance covered at the 4th meeting.
+ *      a) 2:3   b) 3:5  c) 2:5  d) None 
+ *  Ans:    3:5
+ *      -   Ratio of distance covered by Ram and Anuj is always in the ratio 
+ *          of their speeds. So the required ratio = 15: 25 = 3: 5. Hence, 
+ *          option (b) is correct.
+ */
+
+/**
+ *  Time Required
+ *  -   The distance between two cities A and B is 100km and the speeds of 
+ *      Ram and Anuj are 15 km/h and 25km/h respectively. Initially Ram is 
+ *      at A & Anuj is at B. If they move between A and B to and from then 
+ *      answer the question:
+ *  -   The time required for the 1st meeting.
+ *      a) 2h   b) 1h   c) 3h   d) None
+ *  Ans:    None
+ *      -   Distance covered by Ram and Anuj together till the 1st meeting = 100km  
+ *          Total combined speed of Ram and Anuj = 15kmph + 25kmph = 40kmph
+ *          Total time required for the 1st meeting = 100/40 = 2.5 hours. Hence, 
+ *          option (d) is correct.
+ */
+
+/**
+ *  Distance Covered By Anuj
+ *  -   The distance between two cities A and B is 100km and the speeds of Ram 
+ *      and Anuj are 15 km/h and 25km/h respectively. Initially Ram is at A & 
+ *      Anuj is at B. If they move between A and B to and from then answer the 
+ *      following questions:
+ *  -   Distance covered by Anuj till the 3rd meeting.
+ *      a) 250 km   b) 212.5 km     c) 187.5 km  d) 181.5 km 
+ *   Ans:   250 km 
+ */
+
+/**
+ *  Meet for The First Time
+ *  -   A and B are running on a circular track of length 1000 m. Speed of A 
+ *      is 25 m/s and speed of B is 10 m/s. They start from the same point at 
+ *      the same time in the opposite directions. When will they meet for the 
+ *      first time at the starting point again?
+ *      a) 200s     b) 100s c) 50s  d) 250s
+ *  Ans:    200s
+ *      -   Time taken by A to complete one round= 1000/25= 40 s
+ *          Time taken by B to complete one round= 1000/10= 100 s
+ *          First meeting at the starting point will happen after LCM of 
+ *              40 and 100= 200 s.
+ *          Hence they will meet after 200 s. hence, option (a) is correct.
+ */
+
+/**
+ *  When they be together
+ *  -   A, B and C run on a circular path at a speed of 5 m/s, 10 m/s, 20 m/s ,
+ *      A and B are moving in the same direction and C in the opposite direction. 
+ *      The circumference of the path is 6 km. If they start from the same 
+ *      point at the same time then when will they be together again for the 
+ *      1st time?
+ *      a) 1200s    b) 400s     c) 800s     d) 200s
+ *  Ans:    1200s
+ *      -   C meets B after every = 6000/(20+10)=200 s
+ *          C meets A after every = 6000/(20+5)=240 s
+ *          Therefore they will meet each other for the 1st time after= 
+ *              LCM (200, 240) = 1200 s
+ *          Hence, option (a) is correct.
+ */
+
+/**
+ *  For the first time
+ *  -   A, B and C run on a circular path at a speed of 5 m/s, 10 m/s, 20 m/s ,
+ *      A and B are moving in the same direction and C in the opposite direction. 
+ *      The circumference of the path is 6 km. When will they be together 
+ *      again for the 1st time at the starting point?
+ *      a) 1200s    b) 400s     c) 800s     d) 200s
+ *  Ans:    1200s
+ *      -   Time taken by A to complete one round = 6000/5=1200 s
+ *          Time taken by B to complete one round = 6000/10=600 s
+ *          Time taken by C to complete one round = 6000/20=300 s
+ *          So they will be together again for the 1st time at the starting 
+ *          point after:  LCM (1200, 600, 300) = 1200 s. Hence, option (a) 
+ *          is correct.
+ */

ff834d3e5c9b01a8944170aac207291c7f87f21b
“tarunve” <tarun.verma151@gmail.com>
2021-12-08 15:07:23 +0530

    aptitude problems

---
+++ 04-aptitude/modules/013-Blood-Relation-Calenders.cpp
@@ -0,0 +1,189 @@
+/**
+ *  Male Female Problem-1
+ *  -   Answer the questions on the basis of the information given below:
+ *  -   There are six persons in a room namely- A, B, C, D, E and F.A and E are 
+ *      brothers, B and D are daughters of the brother of C’s father. F is the 
+ *      sister of E, C is the only son of A’s uncle.
+ *  -   How many male persons are there?
+ *      a) 5    b) 3    c) 1    d) 0
+ *  Ans:    3
+ */
+
+/**
+ *  Male Female Problem -2
+ *  -   How is C related to F?
+ *      a) Uncle    b) Cousin   c) Aunt     d)  None
+ *  Ans:    Cousin
+ */
+
+/**
+ *  Male Female Problem-3
+ *  -   How many female persons are there?
+ *      a) 0    b) 1    c) 2    d) 3    
+ *  Ans:    3
+ */
+
+/**
+ *  Male Female Problem-4
+ *  -   D is A’s …..?
+ *      a) Uncle    b) Father   c) Cousin     d)  Mother
+ *  Ans:    Cousin
+ */
+
+/**
+ *  Family Problem-1
+ *  -   Direction for next six questions:
+ *  -   A, B, C, D, E and F are six members of a family. C is not the mother 
+ *      of B but B is son of C. A and C are married couple. E is the brother 
+ *      of C. F is the brother of B. D is the daughter of A.
+ *  -   E is D’s _?
+ *      a) Uncle    b) Mother   c) Father   d) Brother
+ *  Ans:    Uncle
+ */
+
+/**
+ *  Family Problem-2
+ *  -   Which of the following is a pair of females?
+ *      a) AD   b) BD   c) CA   d) BA   
+ *  Ans:    AD
+ */
+
+/**
+ *  Family Problem-3
+ *  -   E’s wife is _?
+ *      a)  A   b)  C   c) F    d) can't be determined
+ *  Ans:    Can't be determined
+ */
+
+/**
+ *  Family Problem-4
+ *  -   How many Kids does A have?
+ *      a)  0   b) 1    c) 2    d) 3
+ *  Ans:    3
+ */
+
+/**
+ *  Family Problem-5
+ *  -   How many female members are in the family?
+ *      a) 0    b) 1    c) 2    d) 3
+ *  Ans: 2
+ */
+
+/**
+ *  Family Problem-6
+ *  -   B’s mother is _?
+ *      a) A    b) E    c) F    d) D
+ */
+
+/**
+ *  Predict Day?
+ *  -   July 1, 1777 was Monday. What day of the week was it on July 1, 1778?
+ *      a) Monday   b) Tuesday  c) Wednesday    d)  Sunday
+ *  Ans:    Tuesday
+ *      -   As 1777 is not a leap year hence, it has 1 odd day. So July 1, 1778 
+ *          was one day ahead of the day on July 1, 178. Hence it was Tuesday 
+ *          on July 1, 1778.
+ */
+
+/**
+ *  Which day?
+ *  -   January 1, 2000 was Sunday. What day of the week was it on January 1, 
+ *      2001?
+ *      a) Monday   b) Tuesday  c) Saturday     d) Sunday
+ *  Ans:    Tuesday     
+ *      -   2000 is a leap year hence it has two odd days. Hence, it was Tuesday 
+ *          on January 1, 2001.
+ */
+
+/**
+ *  Predict Day Of Week?
+ *  -   1st Nov, 2003was Sunday. What day of the week was it on 1stDec. 2004?
+ *      a)  Thursday    b)  Sunday  c) Tuesday  d) Friday
+ *  Ans:    Thursday
+ *      -   2004 is a leap year hence if 1 November 2003 was Sunday then 1 
+ *          November 2004 was Sunday+ 2 more days= Tuesday. There are 30 days 
+ *          between 1 November and 1 December. 30 days = 4×7+2.     
+ * 
+ *          Hence day on 1 December 2004 is Tuesday+ 2= Thursday.
+ */
+
+/** 
+ *  Day Of June?
+ *  -   5th June, 2015 was Monday. What was the day of the week on 
+ *      6th June, 2014?
+ *      a) Sunday   b) Saturday     c) Tuesday  d) Monday
+ *  Ans: Monday
+ *      -   As 2015 is not a leap year so day on 5 June 2015= Day on 5 June 
+ *          2014 + 1 Day on 5 June 2014 + 1= Day on 6th June 2015.
+ *          Day on 5th June 2015= Day on 6th June 2014= Monday.
+ */ 
+
+/**
+ *  15 August Day
+ *  -   What was the day on 15 August 1948?
+ *      a)  Friday  b) Saturday     c) Sunday   d) Monday 
+ *  Ans:    Sunday
+ *      -   Odd days in 1600 years = 0 
+ *          Odd days in 300 years = 1
+ *          47 years = (36 ordinary years + 11 leap years) = (36 x 1 + 11x 2)= 
+ *          58(8 weeks + 2 days) = 2 odd day 
+ *          Jan.   Feb.   Mar.   Apr.   May.   Jun.   Jul.   Aug 
+ *          ( 31 + 29 + 31 + 30 + 31 + 30 + 31 + 15 ) = 228 days = (32 weeks +  
+ *          4 days) = 4 odd days.
+ *          Total number of odd days = (0 + 1 + 2 + 4) = 7 odd days. 
+ *          Hence, as the number of odd days = 7, given day is Sunday.
+ */
+
+/**
+ *  Predict Dates?
+ *  -   On what dates of January, 2015 did Sunday fall?
+ *      a) 3rd, 10th, 17th, 24th, 31st  b) 5th, 12th, 19th, 26th
+ *      c) 1st, 8th, 15th, 22nd, 29th   d) 4th, 11th, 18th, 25th 
+ *  Ans:    4th, 11th, 18th, 25th 
+ *      -   2014: 2000 years+ 11 ordinary years+ 3 leap years= 11 odd days+
+ *           6 odd days= 17 odd days or 3 odd days.
+ *          1st January 2015: 3+ 1= 4 odd days.
+ *          Hence, it was Thursday on 1st January 2015. Therefore Sunday 
+ *          fell on 4th, 11th, 18th, 25th  January.
+ */
+
+/**
+ *  Day Of Week again?
+ *  -   What was the day of the week on 25th June, 2016?
+ *      a) Thursday     b) Monday   c) Saturday     d) Sunday 
+ *  Ans:    Saturday
+ *      -   2015= 2000 years+ 3 leap years+ 12 ordinary years= 3×2+12=18 odd 
+ *          days or 4 odd days.
+ *      -   1 January to 25 June= 31+ 29+ 31+ 30+ 31+ 25 or 3+ 1+ 3+ 2+ 3+ 4= 
+ *          16 odd days or 2 odd days.
+ *      -   Therefore total number of odd days= 4 + 2= 6 odd days.
+ *          Hence it was Saturday on 25th June 2016.
+ */
+
+/**
+ *  After few days?
+ *  -   Today is Monday. After 76 days, it will be
+ *      a) Saturday     b) Sunday   c) Monday   d)  Tuesday
+ *  Ans:    Sunday
+ *      -   76= 7×10+6 days. Hence, if today is Monday then after 76 days it
+ *          will be Monday+ 6= Sunday
+ */
+
+/**
+ *  Leap Year
+ *  -   Which of the following is not a leap year?
+ *      a)  1782    b) 1888     c) 1616     d) 2004
+ *  Ans:    1782
+ *      -   As 1782 is not divisible by 4 hence it is not a leap year.
+ */
+
+/**
+ *  Maximum Days
+ *  -   Maximum number of days in 10 consecutive years is:
+ *      a) 3650     b) 3652     c) 3654 d) 3653
+ *  Ans:    3653
+ *      -   10 consecutive years have maximum days if these years consists 
+ *          maximum possible number of leap year. In 10 consecutive years 
+ *          maximum number of leap years = 3 (1st, 5th, 9th or 2nd, 6th& 10th).
+ *          Maximum possible number of days in 10 years= 3×366+7×365=3653 days.
+ */

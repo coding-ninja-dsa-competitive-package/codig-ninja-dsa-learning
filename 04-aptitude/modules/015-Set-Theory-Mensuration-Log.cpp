8b3332a4d849cc707e4cc8a83bea5b76c577217f
“tarunve” <tarun.verma151@gmail.com>
2021-12-09 10:54:22 +0530

    aptitude problems

---
+++ 04-aptitude/modules/015-Set-Theory-Mensuration-Log.cpp
@@ -0,0 +1,268 @@
+/**
+ *  Value of log 6
+ *  -   The value of log 6 is equal to:
+ *      a) log1+log2+log3 b) log(1+2+3) c) log(1*2*3) d) all of the above 
+ *  Ans:    all of the above
+ *      -   log6 = log(1×2×3) = log1 + log2 + log3
+ *          Also log6 = log(1+2+3). Hence, option d is correct.
+ */
+
+/**
+ *  Value of log
+ *  -   Find the value of log(base 8)128.
+ *      a) 7 b) 7/3 c) 5/3 d) None of these
+ *  Ans:    7/3
+ */
+
+/**
+ *  Predict The Value
+ *  -   Find the value of log(base y) x * log(base z)y * log(base x)z
+ *      a) 0 b) 1 c) logx d) xyz
+ *  Ans:    1
+ */
+
+/**
+ *  Find the value?
+ *  -   If (loga/(b-c))=(logb/(c-a))=(logc/(a-b)), then find the value of a^(a) * b^(b) 
+ *      * c^(c).
+ *      a) 0    b) 2    c) 3    d) 1
+ *  Ans:    1
+ *      -   loga/b-c=logb/c-a=logc/a-b=k 
+ *          Then, loga = k(b – c)
+ *          log b = k(c – a)
+ *          logc = k(a – b)
+ *          Let A = a^a.b^b.c^c
+ *          Then logA = aloga + blogb + clogc
+ *              = ak(b – c) + bk(c – a) + ck(a – b)
+ *              = abk – ack + bck – abk + cak – bck = 0
+ *          Thus logA = 0 = log1
+ *          ∴ A = 1
+ *          i.e., a^a.b^b.c^c = 1. Hence, option d is correct.
+ */
+
+/**
+ *  Find the sum?
+ *  -   Find the sum of n terms of the series log(base 3)(p/q) + log(base 9)((p/q)^2) 
+ *      + log(base 27)((p/q)^3) + ……………n terms:
+ *      a) (n+1)log(base 3)(p/q) b) nlog(base 3)(p/q)  c) nlog(base 3)((p/q)^2) 
+ *      d) log(base 3)(p/q)
+ *  Ans;    nlog(base 3)(p/q)
+ */
+
+/**
+ *  Find the value of log?
+ *  -   Find the value of logx + log(x)^4 + log(x)^9 + …………log(x)^(n.n)
+ *      a) (n/6)*(n+1)*(2n+1)   b)  (n/2)*(n+1)*logx c) (n/2)*(n+1) 
+ *      d) (n/6)*(n+1)*(2n+1)*logx 
+ *  Ans:    logx + (logx)^4 + (logx)^9 + …………+ (logx)^(n.n)
+ *          log[x^1* x^4* x^9*…………… n times] .  
+ *          log[x^(1+4+9+……….n.n.)  ]
+ *          log[x^(∑n2).]      log[x^(n(n+1)(2n+1)/6) .]  
+ *              =((n*(n+1)*2n+1)/6)*logx. Option d is correct.
+ */ 
+
+/**
+ *  Number Of Members
+ *  -   In Mindworkzz club all the members like either in Superman or Batman. 320 like 
+ *      in the Batman, 350 like in Superman and 220 like in both. How many members does 
+ *      the club have?
+ *      a) 410 b) 550 c) 440 d) 450 
+ *  Ans:    450
+ */
+
+/**
+ *  How many cleared all three sections?
+ *  -   Refer to the data below and answer the questions that follow.
+ *  -   Last year, there were 3 sections in ASCC, a mock CAT paper. Out of them 33 
+ *      students cleared the cutoff in Section A, 34 students cleared the cutoff in 
+ *      Section B and 32 cleared the cutoff in Section C. 10 students cleared the 
+ *      cutoff in Section A and Section B, 9 cleared the cutoff in Section B and 
+ *      Section C, 8 cleared the cutoff in Section A and Section C. The number of 
+ *      people who cleared each section alone was equal and was 21 for each section.
+ *  -   How many cleared all the three sections?
+ *      a) 7    b) 6    c) 5    d) 8
+ *  Ans:    6
+ */ 
+
+/**
+ *  Above scenario:
+ *  -   How many cleared only one of the three sections?
+ *      a) 21 b) 63 c) 42 d) 52
+ *  Ans:    63  
+ */
+
+/**
+ *  Above scenario:
+ *  -   The ratio of the member of students clearing the cutoff in one or more of the 
+ *      sections to the number of students clearing the cutoff in Section A alone is?
+ *      a) 78/21 b) 3   c) 73/21    d) None
+ *  Ans:    78/21
+ */
+
+/**
+ *  How many surveyed?
+ *  -   A survey of faculty and graduate students at the Mumbai Film Academy revealed 
+ *      the following information: 51 admire Aamir Khan, 49 admire Shahrukh Khan, 60 
+ *      admire Salman Khan, 34 admire Aamir Khan and Shahrukh Khan, 32 admire Shahrukh 
+ *      Khan and Salman Khan, 36 admire Aamir Khan and Salman Khan, 24 admire all three 
+ *      of the Khans and 3 admire none of the three Khans.
+ *  -   How many people were surveyed?  
+ *      a) 82   b) 83   c) 84   d) 85
+ *  Ans:    85
+ */ 
+
+/**
+ *  How many admire?
+ *  -   A survey of faculty and graduate students at the Mumbai Film Academy revealed 
+ *      the following information: 51 admire Aamir Khan, 49 admire Shahrukh Khan, 60 
+ *      admire Salman Khan, 34 admire Aamir Khan and Shahrukh Khan, 32 admire Shahrukh 
+ *      Khan and Salman Khan, 36 admire Aamir Khan and Salman Khan, 24 admire all three 
+ *      of the Khans and 3 admire none of the three Khans.
+ *  -   How many admire Shahrukh, but not Salman or Aamir?
+ *      a)  5   b) 7    c) 16   d) 24
+ *  Ans:    7
+ */
+
+/**
+ *  Admire Either Salman Or Aamir
+ *  -   A survey of faculty and graduate students at the Mumbai Film Academy revealed 
+ *      the following information: 51 admire Aamir Khan, 49 admire Shahrukh Khan, 60 
+ *      admire Salman Khan, 34 admire Aamir Khan and Shahrukh Khan, 32 admire Shahrukh 
+ *      Khan and Salman Khan, 36 admire Aamir Khan and Salman Khan, 24 admire all three     
+ *      of the Khans and 3 admire none of the three Khans.
+ *  -   How many admire either Salman or Aamir but not Shahrukh?
+ *      a) 33   b) 38   c) 43    d) 48
+ *  Ans:    33
+ */
+
+/**
+ *  How many admire not Khans?
+ *  -   A survey of faculty and graduate students at the Mumbai Film Academy revealed 
+ *      the following information: 51 admire Aamir Khan, 49 admire Shahrukh Khan, 60 
+ *      admire Salman Khan, 34 admire Aamir Khan and Shahrukh Khan, 32 admire Shahrukh 
+ *      Khan and Salman Khan, 36 admire Aamir Khan and Salman Khan, 24 admire all three 
+ *      of the Khans and 3 admire none of the three Khans.
+ *  -   How many admire not more than one of the Khans?
+ *      a) 28   b) 31   c) 84   d) 39
+ *  Ans:    31
+ */
+
+/**
+ *  How many learners?
+ *  -   There are 79 Grade 10 learners at school. All of these take some combination of 
+ *      Maths, Geography and History. The number who take Geography is 41; those who take 
+ *      History is 36; and 30 take Maths. The number who take Maths and History is 16; 
+ *      the number who take Geography and History is 6, and there are 8 who take Maths 
+ *      only and 16 who take History only.
+ *  -   How many learners take Maths and Geography but not History?
+ *      a) 6     b) 10  c) 16   d) 22
+ *  Ans:    6
+ */
+
+/**
+ *  Learners Take Geography Only
+ *  -   There are 79 Grade 10 learners at school. All of these take some combination of 
+ *      Maths, Geography and History. The number who take Geography is 41; those who 
+ *      take History is 36; and 30 take Maths. The number who take Maths and History 
+ *      is 16; the number who take Geography and History is 6, and there are 8 who take 
+ *      Maths only and 16 who take History only.
+ *  -   How many learners take Geography only?
+ *      a)  24   b) 29  c) 34   d) 39
+ *  Ans:    29
+ */
+
+/**
+ *  Take All Three Subjects?
+ *  -   There are 79 Grade 10 learners at school. All of these take some combination of 
+ *      Maths, Geography and History. The number who take Geography is 41; those who take 
+ *      History is 36; and 30 take Maths. The number who take Maths and History is 16; 
+ *      the number who take Geography and History is 6, and there are 8 who take Maths 
+ *      only and 16 who take History only.
+ *  -   How many learners take all three subjects?    
+ *      a) 8    b) 6    c) 4    d) 2
+ *  Ans:    2
+ */
+
+/**
+ *  Weight Of Pipe
+ *  -   A hollow iron pipe is 21 cm long and its external diameter is 8 cm. If the 
+ *      thickness of the pipe is 1 cm and iron weighs 8 g/cm3, then the weight of the 
+ *      pipe is:
+ *      a)  3.6 kg b) 36 kg     c) 36.9 kg  d) 3.696 kg
+ *  Ans:    3.696 kg    
+ *      -   External radius = 4 cm
+ *          Internal radius = 4- 1 = 3 cm
+ *          Volume of iron = 22/7*[4^2-3^2]*21=462 cm3
+ *          So the required weight = 8  * 462 = 3696 gm or 3.696 kg. hence, 
+ *          option d is correct.
+ */
+
+/**
+ *  Increase In Area Of Circle
+ *  -   The radius of circle is so increased that its circumference increased by 5%. The 
+ *      area of the circle then increases by
+ *      a) 12.5% b) 10.25% c) 10.5% d) 11.25%
+ *  Ans:    10.25%
+ *      -   Circumference of a circle= 2*3.14* r, where r is the radius.
+ *              Circumference ~ radius
+ *          As the circumference increases by 5%, the radius also increases by 5%
+ *              New radius = 1.05r
+ *              As area ~ (radius)2
+ *              New area = (1.05)^2 old area= 1.1025* old area
+ *          Percentage increase in area = 10. 25%. Hence, option (b) is correct.
+ */
+
+/**
+ *  Mark Option
+ *  -   The area of an isosceles triangle is 12 sq. cm. If one of the equal sides is 5 cm 
+ *      long, mark the option which can give the length of the base.
+ *      a) 4cm b) 5cm c) 8cm    d) 9cm
+ *  Ans:    8cm
+ */
+
+/**
+ *  Volume Of Box
+ *  -   A rectangular piece of cardboard 18cm × 24 cm is made into an open box by 
+ *      cutting a square of 5 cm side from each corner and building up the side. What 
+ *      is the volume of the box?
+ *      a) 480  b) 630  c) 420   d) 560 
+ *  Ans:    560
+ */
+
+/**
+ *  How many cuboids?
+ *  -   How many cuboids of different dimensions can be assembled with 100 identical cubes
+ *      a)  9   b) 8    c) 12   d) 10
+ *  Ans:    8
+ */
+
+/**
+ *  Volume Of Cone
+ *  -   The volume of the largest right circular cone that can be cut out of a cube 
+ *      of edge 7 cm is:
+ *      a)  89.8 cm3 b) 147.68 cm3 c) 13.6 cm3 d) 121 cm3
+ *  Ans:    89.8 cm3
+ */
+
+/**
+ *  By What Percent?
+ *  -   By what percent the volume of a cube increases, if the length of each edge 
+ *      increased by 50%:
+ *      a)  237.5% b) 273.5% c) 125% d) 50%
+ *  Ans:    237.5%
+ */ 
+
+/**
+ *  Length Of Smallest Side
+ *  -   The ratio of sides of a triangle is 3:4:5. If area of the triangle is 72 square 
+ *      unit, then the length of the smallest side is:
+ *      a) 4*(sqrt(3)) unit b) 5*(sqrt(3)) unit c) 6*(sqrt(3)) unit d) 3*(sqrt(3)) unit
+ *  Ans:    6*(sqrt(3)) unit
+ */
+
+/**
+ *  Find the area?
+ *  -   The perimeter of a rhombus is 40 m and its height is 5 m. its area is:
+ *      a) 60 m2 b) 50 m2  c) 45 m2 d) 55 m2
+ *  Ans:    50 m2
+ */

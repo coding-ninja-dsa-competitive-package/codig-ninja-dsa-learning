7155f9c604b5efdce8f9bf00d8420709e2d2f454
“tarunve” <tarun.verma151@gmail.com>
2022-01-25 13:08:57 +0530

    system design problems

---
+++ 06-system-design/code/001-Introduction.cpp
@@ -0,0 +1,73 @@
+/**
+ *  System Design : 
+ *  -   A process of laying down, differnet architectures, different modules, flows,
+ *      systems, to define - how a project/application will be developed.
+ *  -   It comprises of 2 things:
+ *      1.  Low level design 
+ *      2.  High level design
+ * 
+ */
+
+/**
+ *  Choose the correct option
+ *  -   Which of the following is true for high-level design?
+ *  Options
+ *      a.  It is the macro level design, ie. considering the overall system
+ *      b.  Describes the main components of the system architecture
+ *      c.  Low level design get converted into high level design
+ *      d.  The input is software requirements specification (SRS)
+ *  
+ *  Ans :   a,b,d
+ */
+
+/**
+ *  Choose the correct option
+ *  -   Which if the following is true for low-level design?
+ *  Options
+ *      a.  Provides the implementation details of each module
+ *      b.  Describes the overall system into different components
+ *      c.  Required entities and classes are created
+ *      d.  Finalized high level design is the input for this process
+ *   Ans:   a,c,d
+ */
+
+/**
+ *  Choose the correct option
+ *  -   System design involves:
+ *  Options
+ *      a.  Identifying the major components of system
+ *      b.  Develop/Create a software/application/system from scratch
+ *      c.  Identifying current deficiencies and establishing new goals
+ *      d.  All of the above 
+ *  Ans:    d
+ */
+
+/**
+ *  Choose the correct option
+ *  -   Which of the following is not a factor in the failure of the systems 
+ *      developments projects?   
+ *  Options
+ *      a.  size of the company
+ *      b.  inadequate user involvement
+ *      c.  failure of systems integration
+ *      d.  continuation of a project that should have been cancelled
+ *  Ans:    a
+ */
+
+/**
+ *  Choose the correct option
+ *  -   Following are the various phases of life cycle of a software:
+ *      1. Scope Out
+ *      2. Coding
+ *      3. Requirement gathering
+ *      4. Testing
+ *      5. System designing 
+ *      6. Deployment   
+ *      Which of the following arranges them in the correct order?
+ *  Options
+ *      a.  3,1,2,5,6,4
+ *      b.  3,1,5,2,4,6
+ *      c.  1,3,2,5,4,6
+ *      d.  1,3,5,2,4,6
+ *  Ans:    b
+ */

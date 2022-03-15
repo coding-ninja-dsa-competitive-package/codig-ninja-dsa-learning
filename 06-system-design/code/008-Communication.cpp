9f3256b14ededd9614b7f3ef974f353360e0a1ef
“tarunve” <tarun.verma151@gmail.com>
2022-03-15 10:33:00 +0530

    system design problems

---
+++ 06-system-design/code/008-Communication.cpp
@@ -0,0 +1,134 @@
+/**
+ *  Choose the correct option
+ *  -   Which of the following is an example of synchronous communication?
+ *  Options
+ *      a.  Emailing to customer support
+ *      b.  Video call with customer support
+ *  Ans:    b
+ */
+
+/**
+ *  Choose the correct option
+ *  -   Which of the following is true for synchronous communication?
+ *  Options
+ *      a.  A communication session is established by both the sender and the receiver
+ *      b.  A communication session is established by the sender
+ *      c.  A communication session is established by the receiver
+ *      d.  No need to establish a communication session
+ *  Ans:    a
+ */
+
+/**
+ *  Choose the correct option
+ *  -   For which type of applications should we use synchronous communication?
+ *  Options
+ *      a.  Where payment transactions are involved
+ *      b.  Where consistency is not important
+ *      c.  Where real time decision making has to be done
+ *      d.  Where application design has to be kept simple
+ *      e.  Synchronous communication leads to loosely coupled components
+ *  Ans:    a, c, d
+ */
+
+/**
+ *  Choose the correct option
+ *  -   Asynchronous communication is also called start/stop communication
+ *  Options
+ *      a.  True
+ *      b.  False
+ *  Ans:    a
+ *  -   Asynchronous communication is also called start/stop communication because the 
+ *      start and stop bits are used in asynchronous communication as a means of timing 
+ *      or synchronizing the data characters being transmitted. 
+ */
+
+/**
+ *  Assignment
+ *  -   Guess if it is an example of synchronous or asynchronous communication :
+ *      
+ *  Part A
+ *  -   When using online shopping platforms, when a user selects a product, the system 
+ *      generates a query to check if the product is available in inventory or not.
+ *  Ans:    synchronous
+ *  -   Synchronous as the application waits for this response before allowing the user 
+ *      to add the product into the cart.
+ *  
+ *  Part B
+ *  -   When the payment process is going on for the products.
+ *  Ans:    synchronous 
+ *  -   Synchronous as the application waits till the confirmation of payment has been 
+ *      successfully received from the bank.
+ * 
+ *  Part C
+ *  -   After the order has been placed, a confirmation notification is sent on the phone.
+ *  Ans:    asynchronous
+ *  -   Asynchronous, as it can take some time for the notification to arrive, but in the 
+ *      meantime a user can do anything on the application without waiting for the 
+ *      notification.
+ */
+
+/**
+ *  Choose the correct option
+ *  -   Which of the following is true for asynchronous communication?
+ *  Options
+ *      a.  It should be used when the computation time is large
+ *      b.  It is helpful in making systems scalable
+ *      c.  The data is transferred in a steady stream.
+ *      d.  It allows decoupling of components so that they can work independently
+ *      e.  It can reduce the chances of cascading failure of the complete system
+ *  Ans:    a, b, d, e
+ */
+
+/**
+ *  Choose the correct option
+ *  -   Request response pattern is also called tracking id based pattern. State whether
+ *      the following statement is true or false.
+ *  Options
+ *      a.  True 
+ *      b.  False
+ *  Ans:    a
+ */
+
+/**
+ *  Choose the correct option
+ *  -   Which of the following policies is used to deliver messages through the message 
+ *      queue?
+ *  Options
+ *      a.  LIFO
+ *      b.  FIFO
+ *      c.  LRU
+ *  Ans:    b
+ *  -   FIFO or first in first out is used by message queues.
+ */
+
+/**
+ *  Choose the correct option
+ *  -   Which of the following is/are true?
+ *  Options
+ *      a.  Message based communication helps in achieving synchronous communication.
+ *      b.  Messaging is a communication style used to connect different components in a 
+ *          distributed system.
+ *      c.  Message queue can store important data.
+ *      d.  Message queue can help achieve strong consistency for the application
+ *  Ans:    b, d    
+ */
+
+/**
+ *  Choose the correct option
+ *  -   Which of the following is/are true for message queue models?
+ *  Options
+ *      a.  Peer to peer model can have more than 2 machine communicating at a given time
+ *      b.  Publish subscribe model allows multiple machines to receive same message
+ *      c.  Peer to peer model is also known as point to point model
+ *  Ans:    b, c
+ */
+
+/**
+ *  Choose the correct option
+ *  -   Kafka is a tool which provides asynchronous communication solution. State whether 
+ *      the following statement is true or false.
+ *  Options
+ *      a.  True
+ *      b.  False
+ *  Ans:    a
+ */

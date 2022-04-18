94986423e34286ff978943b126be1fc8de78e50d
“tarunve” <tarun.verma151@gmail.com>
2022-04-18 10:55:01 +0530

    dbms problems

---
+++ 07-Introduction-to-DBMS/code/001-Introduction-to-dbms.cpp
@@ -0,0 +1,144 @@
+/**
+ *  MCQ - 1
+ *  -   Data can be used to make decisions only if:
+ *  Options
+ *      a.  It is in huge volume
+ *      b.  It can be processed into information
+ *      c.  It is electronically generated
+ *      d.  It contains numbers
+ *  Ans:    b
+ *  -   Information is required to make decisions and information is nothing but 
+ *      processed data.
+ */
+
+/**
+ *  MCQ - 2
+ *  -   Which of the following is data:
+ *  Options
+ *      a.  Web page
+ *      b.  Image shot from phone
+ *      c.  Fingerprint
+ *      d.  All of the above    
+ *  Ans:    d
+ */
+
+/**
+ *  MCQ - 3
+ *  -   Anything which stores only unstructured data can be called a database.
+ *  Options
+ *      a.  True
+ *      b.  False
+ *  Ans:    b
+ */
+
+/**
+ *  MCQ - 4
+ *  -   Which of the following is not true:
+ *  Options
+ *      a.  Database and Database management system (DBMS) are the same
+ *      b.  Database stores data.
+ *      c.  Database management system is a software.
+ *      d.  All of the above.
+ *  Ans:    a
+ *  -   Database stores data whereas DBMS is a software which helps us in creating, 
+ *      retrieving, manipulating and  managing the database.
+ */
+
+/** 
+ *  MCQ - 5
+ *  -   Which of the following is the purpose of DBMS:
+ *  Options
+ *      a.  Creation of database
+ *      b.  Manipulation of data
+ *      c.  Retrieving data from database
+ *      d.  All of the above
+ *  Ans:    d
+ */
+
+/**
+ *  MCQ - 6
+ *  -   File based systems faces data integrity issues because: 
+ *  Options
+ *      a.  Files contains textual information
+ *      b.  Large data is stored in files
+ *      c.  Duplicate files may have different values
+ *      d.  Files are changed frequently
+ *  Ans:    c
+ */
+
+/** 
+ *  MCQ - 7
+ *  -   What does same data being saved in multiple locations called
+ *  Options
+ *      a.  Inconsistency
+ *      b.  Redundancy
+ *      c.  Integrity
+ *      d.  Security
+ *  Ans:    b
+ */
+
+/**
+ *  MCQ - 8
+ *  -   In File based systems it is easy to maintain concurrency control when multiple 
+ *      users are updating the same file.
+ *  Options
+ *      a.  True
+ *      b.  False
+ *  Ans:    b
+ */
+
+/**
+ *  MCQ - 9
+ *  -   File System in which each teacher of a college is maintaining their separate 
+ *      files concerning the same subjects taught in the course would lead to :
+ *  Options
+ *      a.  High data integrity
+ *      b.  High data redundancy
+ *      c.  High data security
+ *      d.  High efficiency to access data
+ *  Ans:    b
+ */
+
+/**
+ *  MCQ - 10
+ *  -   If your laptop has the application like MS Access which is fetching data from 
+ *      some other system then it is an example of which type of architecture :
+ *  Options
+ *      a.  1 tier
+ *      b.  2 tier
+ *      c.  3 tier
+ *      d.  None of the above.
+ *  Ans:    b
+ */
+
+/**
+ *  MCQ - 11
+ *  -   In a software architecture which of the following is responsible for computing 
+ *      the result and returning it:
+ *  Options
+ *      a.  Client
+ *      b.  Server
+ *  Ans:    b
+ */
+
+/**
+ *  MCQ - 12 ( Data Sharing )
+ *  -   What do you mean by data sharing in DBMS:
+ *  Options
+ *      a.  Same information is visible to everyone
+ *      b.  Different information is visible to everyone    
+ *      c.  Different versions of information is visible to everyone
+ *      d.  Anyone can access data
+ *  Ans:    a
+ */
+
+/**
+ *  MCQ - 13 (Drawback of DBMS)
+ *  -   What is/are the drawback of using DBMS:
+ *  Options
+ *      a.  Requires training
+ *      b.  Complex to use
+ *      c.  It is costly
+ *      d.  All of the above
+ *  Ans:    d
+ */

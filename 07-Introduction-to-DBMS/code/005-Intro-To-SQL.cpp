ce66994d04d033669d6e947ed7f52877868b28d2
tarunve <tarun.verma151@gmail.com>
2022-07-27 20:42:54 +0530

    dbms problems

---
+++ 07-Introduction-to-DBMS/code/005-Intro-To-SQL.cpp
@@ -0,0 +1,46 @@
+/**
+ *  MCQ - 1
+ *  -   Which of the following is true for SQL:
+ *  Options
+ *      a.  SQL is a type of application
+ *      b.  Structured Query Language is a software
+ *      c.  SQL stands for Standard Query Language
+ *      d.  SQL is a programming language 
+ *  Ans:    d
+ *  -   SQL or Structured query language is a programming language that can be used to 
+ *      manipulate,retrieve, delete and store data into  RDBMS.
+ */
+
+/**
+ *  MCQ - 2
+ *  -   Which of the following is true:
+ *  Options
+ *      a.  MySQL and SQL, both are programming language
+ *      b.  MySQL is an open-source DBMS 
+ *      c.  MySQL is a programming language 
+ *      d.  MySQL is a relational DBMS 
+ *  Ans:    b, d
+ *  -   MySQL is an open-source Relational Database Management System.
+ */
+
+/**
+ *  MCQ - 3
+ *  -   Which of the following is true:
+ *  Options
+ *      a.  MYSQL and SQL refer to the same thing which is RDBMS
+ *      b.  MYSQL is a type of RDBMS whereas SQL is a programming language 
+ *      c.  SQL is a type of RDBMS whereas MySQL is a programming language
+ *      d.  None of the above
+ *  Ans:    b
+ *  -   MySQL uses the Client-Server model. In this model, a "client" is a front-end 
+ *      application that uses the services provided by a MySQL server. And this whole 
+ *      use of the services takes place through SQL Queries. The difference between SQL 
+ *      and MySQL is shown below
+ *                  SQL                                 MySQL
+ *      -   SQL is a structured Query Language  |   MySQL is an RDBMS to store, retrieve,
+ *          It is useful to manage relational   |   modify and administrate a DB using SQL.
+ *          databases.                          |
+ *      -   SQL is a query language             |   MySQL is used as an RDBMS database.
+ *      -   To query and operate DB systems     |   Allow data handling, storing, modifying,
+ *                                              |   deleting in a tabular format.
+ */

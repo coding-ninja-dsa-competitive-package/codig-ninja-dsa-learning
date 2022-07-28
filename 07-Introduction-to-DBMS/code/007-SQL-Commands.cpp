4d7e8a22110b3c8174f21cb5008589f43b78ef0e
tarunve <tarun.verma151@gmail.com>
2022-07-28 08:32:07 +0530

    dsms problems

---
+++ 07-Introduction-to-DBMS/code/007-SQL-Commands.cpp
@@ -0,0 +1,53 @@
+/**
+ *  MCQ - 1
+ *  -   Which of the following deals with database schemas and table structure :
+ *  Options
+ *      a.  DDL 
+ *      b.  DML
+ *      c.  DCL
+ *      d.  DQL
+ *  Ans:    a
+ *  -   Data Definition Language: is used to define, alter or delete database schemas 
+ *      and relations.
+ */
+
+/**
+ *  MCQ - 2
+ *  -   Which of the following is true: 
+ *  Options
+ *      a.  DROP command removes the complete table structure from the database 
+ *      b.  TRUNCATE command removes the complete table structure from the database
+ *      c.  TRUNCATE command removes all the tuples from the table but not the table 
+ *      d.  DROP can be used to remove the complete objects such as database, table, 
+ *          views and they cannot be rolled back. 
+ *  Ans:    a, c, d
+ *  -   DROP command can be used to remove complete objects such as databases, tables, 
+ *      views and they cannot be rolled back, so it must be used very carefully.
+ *  -   Truncate command is used to deallocate the contents of columns in a table for 
+ *      reuse, hence it’s fast and reliable and data can be rolled back even after removing.
+ */
+
+/**
+ *  MCQ - 3
+ *  -   ALTER command is a type of:
+ *  Options
+ *      a.  Data Manipulation Language
+ *      b.  Data Definition Language 
+ *      c.  Data Control Language
+ *      d.  None of the above
+ *  Ans:    b
+ *  -   ALTER commands modifies the structure of the table and hence is a kind of Data 
+ *      Definition Language.
+ */
+
+/**
+ *  MCQ - 4
+ *  -   Which of the following is not a part of Data Manipulation Language :
+ *  Options
+ *      a.  INSERT
+ *      b.  ROLLBACK 
+ *      c.  UPDATE
+ *      d.  DELETE
+ *  Ans:    b
+ *  -   Rollback is a Transaction Control Language command.
+ */

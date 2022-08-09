037c448936905f8b788e874ed0379d5c60ba18ed
tarunve <tarun.verma151@gmail.com>
2022-08-10 04:54:54 +0530

    dbms problems

---
+++ 07-Introduction-to-DBMS/code/021-NOSQL.cpp
@@ -0,0 +1,474 @@
+/** 
+ *  MCQ - 1 
+ *  -   NoSQL databases are used usually for handling large volumes of ______________ data.
+ *  Options
+ *      a.  Structured
+ *      b.  Semi-structured
+ *      c.  Unstructured
+ *      d.  All of the above
+ *  Ans:    c
+ *  -   NoSQL is popularly misunderstood as No SQL but it actually means Not only SQL 
+ *      database. NoSQL is also claimed as the next-generation database. The Relational 
+ *      databases we use are not designed to manage all kinds of data efficiently, like 
+ *      large volumes of unstructured data whereas NoSQL databases are designed to manage 
+ *      these data efficiently.
+ */
+
+/** 
+ *  MCQ - 2
+ *  -   What is true for NoSQL database ?
+ *  Options
+ *      a.  NoSQL cannot store structured data
+ *      b.  NoSQL is a database that is built on ways and means to store data in format 
+ *          other than tables.
+ *      c.  NoSQL uses tables to store data and is an enhanced form of RDBMS.
+ *      d.  None
+ *  Ans:    b
+ *  -   NoSQL is a schema-free database. The data structure is used for storing is 
+ *      different from what we learned in relational databases. Data structures used here 
+ *      are more flexible than the relation used in relational databases. Also,  the data 
+ *      is not stored in a table format. In NoSQL, data is stored in document databases, 
+ *      key-value stores, wide-column databases, and graph databases.
+ */
+
+/** 
+ *  MCQ - 3
+ *  -   NoSQL stands for:
+ *  Options
+ *      a.  Advance SQL
+ *      b.  Not SQL
+ *      c.  Not Only SQL
+ *      d.  New Optimized SQL
+ *  Ans:    c
+ *  -   NoSQL, popularly misunderstood as No-SQL but it actually means Not Only SQL database. 
+ */
+
+/** 
+ *  MCQ - 4 
+ *  -   NoSQL databases are often referred to as:
+ *  Options
+ *      a.  Object-oriented databases
+ *      b.  Network databases
+ *      c.  Relational databases
+ *      d.  Distributed databases
+ *  Ans:    d
+ *  -   NoSQL Databases are referred to as distributed databases. It's a mechanism for 
+ *      storing and retrieving data and is also claimed as the next-generation database. 
+ *      They are used in real-time web applications and big data and their usage is peaking 
+ *      over time. NoSQL databases go against the conventional attitude of storing 
+ *      information at a single location, instead, it distributes and stores information 
+ *      over a set of multiple servers.
+ */
+
+/** 
+ *  MCQ - 5 
+ *  -   Most NoSQL databases support automatic __________ meaning that you get high 
+ *      availability and disaster recovery.
+ *  Options
+ *      a.  processing
+ *      b.  scalability
+ *      c.  replication
+ *      d.  all of the mentioned
+ *  Ans:    c
+ *  -   NoSQL databases are highly available due to their auto replication feature i.e. 
+ *      whenever any kind of failure happens data replicates itself to the preceding 
+ *      consistent state. This helps in high availability and data recovery.
+ */
+
+/** 
+ *  MCQ - 6 
+ *  -   What do we call a phenomenon when we add more computational power to our existing 
+ *      machine?
+ *  Options
+ *      a.  Sharding
+ *      b.  Scale up
+ *      c.  Horizontal Scaling
+ *      d.  None of the above
+ *  Ans:    b
+ *  -   Explanation:  Vertical scaling means that you scale by adding more power (CPU, RAM) 
+ *      to an existing machine. It is also known as Scaling up.
+ */
+
+/** 
+ *  MCQ - 7 
+ *  -   What are the advantages of NoSQL?
+ *  Options
+ *      a.  It supports semi-structured data and volatile data.
+ *      b.  Read/Write throughput is very high.
+ *      c.  Flexible Schema
+ *      d.  Can scale up to great extent
+ *      e.  All of the above    
+ *      f.  None    
+ *  Ans:    e
+ *  -   Advantages of No SQL are
+ *      1. Horizontal Scaling: Scaling out means distributing the database throughout 
+ *      multiple computers when the load increases. Therefore, it can handle more traffic 
+ *      simply by adding more servers to the database. It can scale up to great extent.
+ *      2. High Availability: NoSQL databases are highly available due to their auto 
+ *      replication feature i.e. whenever any kind of failure happens data replicates 
+ *      itself to the preceding consistent state. It is available for all types of data 
+ *      including semi-structured and volatile data.
+ *      3.Less management:  NoSQL databases require minimum to zero management, they are 
+ *      equipped to auto-repair data distribution and due to it having flexible data models 
+ *      that result in reducing administration and performance difficulties. This results 
+ *      in very high throughput in read/write operations
+ *      4.Flexible Schema: Relational databases are used to have a defined schema, which 
+ *      is quite an issue because in case you need to make any modification or addition to 
+ *      the database we need to change the schema as well every time.
+ */
+
+/** 
+ *  MCQ - 8 
+ *  -   What does RDBMS ensures but NoSQL doesn’t ?
+ *  Options
+ *      a.  ACID properties
+ *      b.  No Redundancy
+ *      c.  Both
+ *      d.  None
+ *  Ans:    c
+ *  -   When compared to SQL or relational databases, NoSQL database is designed for 
+ *      storing data of many types but it lacks functionalities like it doesn’t support 
+ *      transaction properties like ACID, etc.
+ *  -   Data Redundancy means having the same copies of data in a database. It is not 
+ *      accomplished in NoSQL but it is done in RDBMS.
+ */
+
+/** 
+ *  MCQ - 9 
+ *  -   Which of the following is usually not a feature in NoSQL?
+ *  Options
+ *      a.  Easy scaling        
+ *      b.  Creating fixed schema
+ *      c.  High availability
+ *      d.  None
+ *  Ans:    b
+ *  -   Relational databases are used to have a defined schema, which is quite an issue 
+ *      because in case you need to make any modification or addition to the database we 
+ *      need to change the schema as well every time. To overcome this NoSQL creates a 
+ *      flexible schema.    
+ */
+
+/** 
+ *  MCQ - 10 
+ *  -   What are the disadvantages of NoSQL?
+ *  Options
+ *      a.  In order to support ACID developers will have to implement their own code, 
+ *          making their systems more complex.
+ *      b.  If the data requirements are not clear, we can use a flexible schema.
+ *      c.  NoSQL databases don’t have the reliability functions which Relational Databases 
+ *          have.
+ *      d. a,b
+ *      e. a,c
+ *      f. All of the above
+ *      g. None
+ *  Ans:    e
+ *  -   The disadvantages of NoSQL:
+ *      1.  When compared to SQL or relational databases, a NoSQL database is designed for 
+ *          storing data of many types but it lacks functionality like it doesn’t support 
+ *          transaction properties like ACID. 
+ *      2.  The process of managing big data on NoSQL is quite complex as compared to RDBMS. 
+ *      3.  It doesn’t support data entry with constraints like RDBMS
+ *      4.  Other concerns for NoSQL are standardization, Interfaces and Interoperability 
+ *          and also less community support.
+ *      5.  It is also not compatible with SQL, although few NoSQL databases do use SQL but 
+ *          many don’t.
+ */
+
+/** 
+ *  MCQ - 11
+ *  -   Redis is a database type based on:
+ *  Options
+ *      a.  SQL
+ *      b.  Key-value NoSQL
+ *      c.  Relational data model
+ *      d.  JSON Database
+ *  Ans:    b
+ *  -   A key-value pair consists of a value that is basically any piece of data or 
+ *      information is saved with a key to identify its location at the time of operation. 
+ *      Database which stores data in the form of key-value pair is known as key-value 
+ *      No-SQL database. Redis is a popular key-value NoSQL database. The other examples 
+ *      of key-value databases are Amazon AWS, DynamoDB, Oracle NoSQL, Aerospike.
+ */
+
+/** 
+ *  MCQ - 12
+ *  -   Key-value NoSQL, is designed for managing:
+ *  Options
+ *      a.  Associative arrays
+ *      b.  Unstructured data
+ *      c.  Json data files
+ *      d.  All of them
+ *  Ans:    d
+ *  -   Explanation: In Key-Value database, we can store any type of data in the value. 
+ */
+
+/** 
+ *  MCQ - 13
+ *  -   Key Value database should not be used, if there are:
+ *  Options
+ *      a.  Heavy updates on the data.
+ *      b.  Small data chunks of different format to be stored.
+ *      c.  Heavy reads on the data.
+ *      d.  None
+ *  Ans:    a
+ *  -   When there are heavy write operations (insert, update, delete, etc) on the database 
+ *      key-value pair database should not be used because of the reason that query is 
+ *      performed on one key at a time. It takes a lot of time to do heavy write operations 
+ *      on the database. It's difficult to find the key: value pair we want to update 
+ *      because we cannot search using the value.
+ */
+
+/** 
+ *  MCQ - 14
+ *  -   In what database, while recording the data, the timestamp of entering that data is 
+ *      also recorded.
+ *  Options
+ *      a.  Relational Database with a counter
+ *      b.  Key-value database
+ *      c.  Column based NoSQL database
+ *      d.  All of them
+ *  Ans:    c
+ *  -   Column based No-SQL  database records the timestamp of data, while recording 
+ *      the database.
+ */
+
+/** 
+ *  MCQ - 15
+ *  -   What is a unique identifier of the data in a columnar database?
+ *  Options
+ *      a.  Row id
+ *      b.  Key
+ *      c.  Timestamp
+ *      d.  Can be any column
+ *  Ans:    a
+ *  -   Columnar NoSQL Database stores data in columns instead of rows. It speeds up the 
+ *      read and write process from the memory to return a query faster. It stores data 
+ *      in a way that greatly improves disk I/O performance. So, the unique identifier in 
+ *      the data become the row_id of the database
+ */
+
+/** 
+ *  MCQ - 16
+ *  -   Which property of the columnar database makes it apt for a content management system.
+ *  Options
+ *      a.  Aggregation
+ *      b.  Scalability
+ *      c.  Flexible
+ *      d.  Compression
+ *  Ans:    d
+ *  -   Explanation: Compression: Column stores are very efficient at data compression and 
+ *      this is why it is used for content management systems.
+ */
+
+/** 
+ *  MCQ - 17
+ *  -   In Document based NoSQL database, data models contains:
+ *  Options
+ *      a.  Tables
+ *      b.  Documents
+ *      c.  Key value pair  
+ *      d.  None
+ *  Ans:    b
+ *  -   Document Based NoSQL database are very similar to key-value databases. Here 
+ *      information is stored in a document along with a key pair. It uses the internal 
+ *      structure of the document for identification and storage. The data is saved as an 
+ *      instance in the database in comparison to how we do it in relational databases i.e. 
+ *      in tabular form. This method of storing data makes it easier for users to map the 
+ *      data in the database
+ */
+
+/** 
+ *  MCQ - 18
+ *  -   Which of the following implements ACID transactions:
+ *  Options
+ *      a. Key-value Nosql database 
+ *      b. Relational database
+ *      c. Document based nosql database
+ *      d. b and c
+ *  Ans:    d
+ *  -   ACID property is implemented by: Relational as well as Document-based NoSQL 
+ *      database. Key-value pair doesn’t implement ACID properties.
+ */
+
+/** 
+ *  MCQ - 19
+ *  -   eBay has many of its projects running on MongoDB, which kind of database is 
+ *      being used?
+ *  Options
+ *      a.  Columnar Nosql database
+ *      b.  Key-value Nosql database
+ *      c.  Document based nosql database
+ *      d.  none
+ *  Ans:    c
+ *  -   MongoDB is a type of Document type database. It stores data in the form of JSON 
+ *      format. MongoDB is developed by MongoDB Inc. and licensed under the Server Side 
+ *      Public License. Some real-life applications of document-based databases:
+ *      1. Blogging sites like Twitter 
+ *      2. Analytical platforms 
+ *      3. E-commerce platforms like Amazon, eBay.  
+ *      4. Content management systems.
+ */
+
+/** 
+ *  MCQ - 20
+ *  -   Which of the following databases contains edges and nodes?
+ *  Options
+ *      a.  Columnar NoSQL database
+ *      b.  Key-value NoSQL database
+ *      c.  Document based NoSQL database
+ *      d.  Graph based NoSQL database
+ *  Ans:    d
+ *  -   Graph Based NoSQL is a collection of related objects. Each node in the graph 
+ *      represents an entity, where all of them are interconnected, the edge through which 
+ *      nodes are connected defines relationships among them. 
+ */
+
+/** 
+ *  MCQ - 21
+ *  -   When we need to store a database of social websites like Facebook, what database 
+ *      should be used?
+ *  Options
+ *      a.  Columnar NoSQL database
+ *      b.  Graph based NoSQL database
+ *      c.  Key-value NoSQL database
+ *      d.  Document based NoSQL database
+ *  Ans:    b
+ *  -   Real-Life applications where Graph-based database is used:
+ *      1. It can work really well when working on social networks.
+ *      2. It can be used to support transportation systems as well. 
+ *      3. It can be used to detect fraud in transactions. 
+ *      4. It can be used to store criminal network data.
+ */
+
+/** 
+ *  MCQ - 22
+ *  -   Which of the following can be stored using graph-based NoSQL database?
+ *  Options
+ *      a.  Medical history of a person
+ *      b.  Fraud Detection
+ *      c.  Recommendation engine
+ *      d.  Network and IT operations
+ *  Ans:    a, b, c, d
+ *  -   The graph-based representation will help understand all these situations better 
+ *      because it will help define the relationship (if any). Like for network and IT 
+ *      operations, how are employees connected. Professionals these days actively use 
+ *      Linkedin to connect.
+ *  -   For the Recommendation Engine, it will depend upon the searches/purchases we have 
+ *      made, a relationship will be established between them and hence recommendations 
+ *      will be made.
+ *  -   For the medical history of a person, there could be cases where certain diseases 
+ *      make a person prone to other diseases as well. So that relationship could be 
+ *      established. It could also be used to explain a condition if anything is being 
+ *      inherited in the genes. 
+ *  -   For Fraud detection, it works if any odd amount of money is being withdrawn from 
+ *      the account. Now to get that judgement of odd, the database contains previous all 
+ *      the transactions made, along with the time they were made.
+ *  -   These form a relationship in-between them and if something is observed out of 
+ *      trend/rules of relationship, fraud is detected.
+ *  -   Any other database will make this process of forming relationships very tedious, 
+ *      hence graph-based databases are used which are quite simple to comprehend when 
+ *      talking about relationships among entities.
+ */
+
+/** 
+ *  MCQ - 23
+ *  -   Out of the following, which is an apt reason to use an SQL database?
+ *  Options
+ *      a.  It can easily store unstructured data.
+ *      b.  It can enable development in the cloud
+ *      c.  It's ACID-compliant
+ *      d.  None
+ *  Ans:    c
+ *  -   Some of the reasons why we should use SQL databases.
+ *      1.  SQL databases are long-established with a fixed schema design and a set 
+ *          structure.
+ *      2.  SQL databases are ideal for applications that require multi-row transactions 
+ *          such as an accounting system or for legacy systems that were built for a 
+ *          relational structure.
+ *      3.  SQL databases are in accordance with ACID properties.
+ */
+
+/** 
+ *  MCQ - 24
+ *  -   Which of the following databases is ideal for being used for User’s session data 
+ *      retrieval?
+ *  Options
+ *      a.  Columnar Nosql database
+ *      b.  Graph based nosql database
+ *      c.  Key-value Nosql database
+ *      d.  Document based nosql database
+ *  Ans:    c
+ *  -   Explanation: Key-value stores are ideal for storing and retrieving session data at 
+ *      high speeds. The unique Id generated by cookies act as a key while the other 
+ *      information such as user profiles act as a value.
+ */
+
+/** 
+ *  MCQ - 25
+ *  -   Which situation from the following will be apt to use key-value stores?
+ *  Options     
+ *      a.  Blogging
+ *      b.  Managing Web Advertisements
+ *      c.  Google maps
+ *      d.  Manage data warehouse   
+ *  Ans:    b
+ *  -   Explanation: Key-Value databases are mainly used by web advertisement companies. 
+ *      On the basis of users' online activity, web advertisement companies decide which 
+ *      advertisement to show to the user. It is also important to note that serving 
+ *      advertisements should be fast enough. It is important to target the right 
+ *      advertisement to the right customer in order to receive more clicks and hence to 
+ *      maximize the profits.
+ *  -   Combination of factors such as user’s tracked activity online, language and 
+ *      location determine what a user is interested in forms the key while as all other 
+ *      factors that are needed to serve the advertisement better are kept as the value 
+ *      in key-value databases.
+ */
+
+/** 
+ *  MCQ - 26
+ *  -   Which database is a smart choice for data warehousing and big data processing?
+ *  Options
+ *      a.  Columnar Nosql database
+ *      b.  Graph based nosql database
+ *      c.  Key-value Nosql database
+ *      d.  Document based nosql database
+ *  Ans:    a
+ *  -   Explanation: A columnar database stores data by columns rather than by rows, which 
+ *      makes it suitable for analytical query processing, faster retrieval and thus for 
+ *      data warehouses
+ */
+
+/** 
+ *  MCQ - 27
+ *  -   Which of the following databases will be the best choice to maintain the Images 
+ *      data if required for any sort of project?
+ *  Options
+ *      a.  Columnar Nosql database
+ *      b.  Graph based nosql database
+ *      c.  Key-value Nosql database
+ *      d.  Document based nosql database
+ *  Ans:    d
+ *  -   Explanation: CouchDB, will contain a bucket for the JSON documents containing 
+ *      the metadata about each image. 
+ *  -   Another bucket can also be used to store the thumbnail of the image which might 
+ *      work as key for the data data structure.
+ */
+
+/** 
+ *  MCQ - 28
+ *  -   Out of the following, for which situation a graph based database will be apt?   
+ *  Options
+ *      a.  Semantic Search
+ *      b.  Leaderboard for online games
+ *      c.  Content management
+ *      d.  Shopping cart at any online e-commerce website.
+ *  Ans:    a
+ *  -   Explanation: Semantic search is search with meaning, as opposed to “normal” search 
+ *      where the search engine looks for literal matches of the queried words without 
+ *      understanding the overall meaning of the query.
+ *  -   It takes into account the context of search, location and the intent of queries. 
+ *      It understands the searcher’s intent and the contextual meaning of terms in the 
+ *      Web, or on an enterprise data storage, and provides more relevant results.
+ *  -   Hence, graph based database will be able to workout these relationships and 
+ *      come up with better results.
+ */
